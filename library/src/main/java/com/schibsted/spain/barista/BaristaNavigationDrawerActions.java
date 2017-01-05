99be8fde67769bd5dbeaa7bd8ad2fbb7650107ec
Savino Ordine <savino.ordine@scmitaly.it>
2017-01-05 23:36:45 +0100

    create drawer actions

---
+++ library/src/main/java/com/schibsted/spain/barista/BaristaNavigationDrawerActions.java
@@ -0,0 +1,15 @@
+package com.schibsted.spain.barista;
+
+import android.support.annotation.IdRes;
+import android.support.test.espresso.contrib.DrawerActions;
+
+public class BaristaNavigationDrawerActions {
+
+    public static void openDrawer(@IdRes int id) {
+        DrawerActions.openDrawer(id);
+    }
+
+    public static void closeDrawer(@IdRes int id) {
+        DrawerActions.closeDrawer(id);
+    }
+}
