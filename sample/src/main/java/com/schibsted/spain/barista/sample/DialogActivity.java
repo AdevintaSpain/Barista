b41a8113f4236fd83dad19aac4b34e8c089d32c3
Savino Ordine <savino.ordine@scmitaly.it>
2017-01-06 15:01:12 +0100

    create dialog activity

---
+++ sample/src/main/java/com/schibsted/spain/barista/sample/DialogActivity.java
@@ -0,0 +1,52 @@
+package com.schibsted.spain.barista.sample;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DialogActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+
+        final TextView textView = (TextView) findViewById(R.id.dialog_selected_button_value);
+
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setTitle("Dialog")
+
+                .setPositiveButton("positive", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textView.setText("positive");
+                    }
+                })
+                .setNegativeButton("negative", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textView.setText("negative");
+                    }
+                })
+                .setNeutralButton("neutral", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textView.setText("neutral");
+                    }
+                });
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.create().show();
+            }
+        });
+    }
+}
