dbb0e5fc03a3c41875401913141f46e17f92891a
Savino Ordine <savino.ordine@scmitaly.it>
2017-01-10 14:07:53 +0100

    removed assertion from actions class

--- library/src/main/java/com/schibsted/spain/barista/BaristaNavigationDrawerActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaNavigationDrawerActions.java
@@ -3,12 +3,6 @@ package com.schibsted.spain.barista;
 import android.support.annotation.IdRes;
 import android.support.test.espresso.contrib.DrawerActions;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
-import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 public class BaristaNavigationDrawerActions {
 
     public static void openDrawer(@IdRes int id) {
@@ -17,13 +11,5 @@ public class BaristaNavigationDrawerActions {
 
     public static void closeDrawer(@IdRes int id) {
         DrawerActions.closeDrawer(id);
-    }
-
-    public static void drawerIsOpen(@IdRes int id) {
-        onView(withId(id)).check(matches(isOpen()));
-    }
-
-    public static void drawerIsClosed(@IdRes int id) {
-        onView(withId(id)).check(matches(isClosed()));
     }
 }
--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/NavigationDrawerActivityTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/NavigationDrawerActivityTest.java
@@ -7,11 +7,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.schibsted.spain.barista.BaristaAssertions.assertDrawerIsClosed;
+import static com.schibsted.spain.barista.BaristaAssertions.assertDrawerIsOpen;
 import static com.schibsted.spain.barista.BaristaAssertions.assertTextIsDisplayed;
 import static com.schibsted.spain.barista.BaristaAssertions.assertTextIsNotDisplayed;
 import static com.schibsted.spain.barista.BaristaNavigationDrawerActions.closeDrawer;
-import static com.schibsted.spain.barista.BaristaNavigationDrawerActions.drawerIsClosed;
-import static com.schibsted.spain.barista.BaristaNavigationDrawerActions.drawerIsOpen;
 import static com.schibsted.spain.barista.BaristaNavigationDrawerActions.openDrawer;
 
 
@@ -25,10 +25,10 @@ public class NavigationDrawerActivityTest {
     public void openAndCloseDrawer() {
         openDrawer(R.id.drawer_layout);
         assertTextIsDisplayed("menu item");
-        drawerIsOpen(R.id.drawer_layout);
+        assertDrawerIsOpen(R.id.drawer_layout);
 
         closeDrawer(R.id.drawer_layout);
         assertTextIsNotDisplayed("menu item");
-        drawerIsClosed(R.id.drawer_layout);
+        assertDrawerIsClosed(R.id.drawer_layout);
     }
 }
