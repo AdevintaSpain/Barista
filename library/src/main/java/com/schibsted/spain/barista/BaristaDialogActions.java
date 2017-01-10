25ab0f5de16dd573cb08c9a05814891946284a6e
Roc Boronat <roc@fewlaps.com>
2017-01-10 17:20:02 +0100

    Use Barista's click on the Dialog Action to fix a failing test

--- library/src/main/java/com/schibsted/spain/barista/BaristaDialogActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaDialogActions.java
@@ -1,21 +1,19 @@
 package com.schibsted.spain.barista;
 
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.schibsted.spain.barista.BaristaClickActions.click;
 
 public class BaristaDialogActions {
 
     public static void clickDialogPositiveButton() {
-        onView(withId(android.R.id.button1)).perform(click());
+        click(android.R.id.button1);
     }
 
     public static void clickDialogNegativeButton() {
-        onView(withId(android.R.id.button2)).perform(click());
+        click(android.R.id.button2);
     }
 
     public static void clickDialogNeutralButton() {
-        onView(withId(android.R.id.button3)).perform(click());
+        click(android.R.id.button3);
     }
 }
