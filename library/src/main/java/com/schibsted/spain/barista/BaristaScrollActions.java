5b6429c3f4b2209521e9987d32c0aae071a6b8be
Roc Boronat <roc@fewlaps.com>
2017-01-10 16:04:29 +0100

    Repeat the scroll action several times to fix an Espresso bug related to scrolls

--- library/src/main/java/com/schibsted/spain/barista/BaristaScrollActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaScrollActions.java
@@ -1,20 +1,44 @@
 package com.schibsted.spain.barista;
 
 import android.support.annotation.IdRes;
+import android.support.test.espresso.PerformException;
 import android.support.test.espresso.action.ViewActions;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+/**
+ * Scrolls in Espresso are not as great as we could except. For that reason, we will try
+ * to scroll several times instead of just one. If any of them worked, we will share the
+ * Espresso's Exception to the caller.
+ */
 public class BaristaScrollActions {
 
+    private static final int MAX_SCROLL_ATTEMPTS = 100;
+
     public static void scrollTo(@IdRes int id) {
-        onView(withId(id)).perform(ViewActions.scrollTo());
+        for (int i = 0; i <= MAX_SCROLL_ATTEMPTS; i++) {
+            try {
+                onView(withId(id)).perform(ViewActions.scrollTo());
+            } catch (PerformException exception) {
+                if (i == MAX_SCROLL_ATTEMPTS) {
+                    throw exception;
+                }
+            }
+        }
     }
 
     public static void scrollTo(String text) {
-        onView(withText(text)).perform(ViewActions.scrollTo());
+        for (int i = 0; i <= MAX_SCROLL_ATTEMPTS; i++) {
+            try {
+                onView(withText(text)).perform(ViewActions.scrollTo());
+            } catch (PerformException exception) {
+                if (i == MAX_SCROLL_ATTEMPTS) {
+                    throw exception;
+                }
+            }
+        }
     }
 
 }
