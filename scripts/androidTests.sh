1a472cc7145c8ca20244a7aa542536ef2486f68f
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-01-26 01:24:35 +0100

    Fixed test apk path

--- scripts/androidTests.sh
+++ scripts/androidTests.sh
@@ -14,4 +14,4 @@ wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk
 tar xf google-cloud-sdk-127.0.0-linux-x86_64.tar.gz
 echo "y" | ./google-cloud-sdk/bin/gcloud components update beta
 ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file secret.json
-./google-cloud-sdk/bin/gcloud beta test android run --type instrumentation --app ./sample/build/outputs/apk/sample-debug.apk --test ./sample/build/outputs/apk/sample-debug-androidTest.apk --device-ids Nexus5 --os-version-ids 22 --locales en --orientations portrait --project ${PROJECT_ID}  --results-history-name "${HISTORY_NAME}"
+./google-cloud-sdk/bin/gcloud beta test android run --type instrumentation --app ./sample/build/outputs/apk/sample-debug.apk --test ./sample/build/outputs/apk/sample-debug-androidTest-unaligned.apk --device-ids Nexus5 --os-version-ids 22 --locales en --orientations portrait --project ${PROJECT_ID}  --results-history-name "${HISTORY_NAME}"
