1af0e7cf99e331e4d359af58b562a2e5c9e401e7
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-01-27 11:49:05 +0100

    Removed hisotry parameter
    It doesn't work as we expected

--- scripts/androidTests.sh
+++ scripts/androidTests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 PROJECT_ID="barista-tests"
-if [[ $TRAIVS_PULL_REQUEST != "false" ]]; then
-  HISTORY_NAME="PR#${TRAIVS_PULL_REQUEST}"
-else
-  HISTORY_NAME="${TRAIVS_BRANCH}"
-fi
-
 
 set -v
 
@@ -14,4 +8,4 @@ wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk
 tar xf google-cloud-sdk-127.0.0-linux-x86_64.tar.gz
 echo "y" | ./google-cloud-sdk/bin/gcloud components update beta
 ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file secret.json
-./google-cloud-sdk/bin/gcloud beta test android run firebase-test.yml:test-suite --project ${PROJECT_ID}  --results-history-name "${HISTORY_NAME}"
+./google-cloud-sdk/bin/gcloud beta test android run firebase-test.yml:test-suite --project ${PROJECT_ID}
