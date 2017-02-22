0012de94e3b3fe83e5008aaee54f8cacdf7030e8
Roc Boronat <roc@fewlaps.com>
2017-02-22 15:08:00 +0100

    Add the Exception to Log.e, to give more information about the issue

--- library/src/main/java/com/schibsted/spain/barista/permission/PermissionGranter.java
+++ library/src/main/java/com/schibsted/spain/barista/permission/PermissionGranter.java
@@ -34,7 +34,7 @@ public class PermissionGranter {
         }
       }
     } catch (UiObjectNotFoundException e) {
-      Log.e("Barista", "There is no permissions dialog to interact with");
+      Log.e("Barista", "There is no permissions dialog to interact with", e);
     }
   }
 
