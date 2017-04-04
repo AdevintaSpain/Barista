535108e7b65cdfa6afa0bc44cf7c229b76957136
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-04-04 12:57:08 +0200

    Clear Database Test Rule (#29)
    * Implemented ClearDatabaseRule
    
    Creates Activity and test to check if database is cleared
    
    * Fixed PMD violation
    
    * Fixed checkstyle violations
    
    * Renamed SqliteOpenHelper for clarity
    
    * Extracted database version to a constant for clarity
    
    * Changed database name to help identify it
    
    * Added ClearDatabaseRule to the readme

---
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/ClearDatabaseRuleTest.java
@@ -0,0 +1,49 @@
+package com.schibsted.spain.barista.sample;
+
+import android.support.test.rule.ActivityTestRule;
+import com.schibsted.spain.barista.BaristaClickActions;
+import com.schibsted.spain.barista.cleardata.ClearDatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.schibsted.spain.barista.BaristaAssertions.assertDisplayed;
+
+public class ClearDatabaseRuleTest {
+
+  @Rule
+  public ActivityTestRule<DatabaseActivity> activityRule = new ActivityTestRule<>(DatabaseActivity.class, true, false);
+
+  @Rule
+  public ClearDatabaseRule clearDatabaseRule = new ClearDatabaseRule();
+
+  //
+  // Only one of these two tests will succeed when database is not cleared.
+  // They rely on database being cleared between test executions.
+  //
+
+  @Test
+  public void checkOnceThatValueIsZeroFistAndOneAfterIncrement() throws Exception {
+    activityRule.launchActivity(null);
+
+    assertCurrentValueIs("0");
+    incrementValue();
+    assertCurrentValueIs("1");
+  }
+
+  @Test
+  public void checkTwiceThatValueIsZeroFistAndOneAfterIncrement() throws Exception {
+    activityRule.launchActivity(null);
+
+    assertCurrentValueIs("0");
+    incrementValue();
+    assertCurrentValueIs("1");
+  }
+
+  private void assertCurrentValueIs(String expectedValue) {
+    assertDisplayed(expectedValue);
+  }
+
+  private void incrementValue() {
+    BaristaClickActions.click(R.id.database_increment_button);
+  }
+}
