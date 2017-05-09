9167eea74db1af0d2ad5165614b54c6ee3b3c4f4
Roc Boronat <roc@fewlaps.com>
2017-05-09 11:43:19 +0200

    Avoid taking in consideration the position of the item in the nested RecyclerView tests

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/RecyclerViewInsideViewPagerTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/RecyclerViewInsideViewPagerTest.java
@@ -8,34 +8,33 @@ import org.junit.runner.RunWith;
 
 import static com.schibsted.spain.barista.BaristaAssertions.assertDisplayed;
 import static com.schibsted.spain.barista.BaristaRecyclerViewActions.clickRecyclerViewItem;
+import static com.schibsted.spain.barista.BaristaRecyclerViewActions.clickRecyclerViewItemChild;
 import static com.schibsted.spain.barista.BaristaRecyclerViewActions.scrollTo;
 
 @RunWith(AndroidJUnit4.class) public class RecyclerViewInsideViewPagerTest {
 
+  int POSITION_AT_LAST_PAGE = 60;
+
   @Rule public ActivityTestRule<RecyclerViewsInsideViewPagerActivity> activityRule =
       new ActivityTestRule<>(RecyclerViewsInsideViewPagerActivity.class);
 
-  @Test public void checkClickRecyclerViewItem_atSixty() {
-    int position = 60;
-    clickRecyclerViewItem(R.id.recycler, position);
+  @Test public void checkClickRecyclerViewItem() {
+    clickRecyclerViewItem(R.id.recycler, POSITION_AT_LAST_PAGE);
     assertDisplayed("Papaya has been clicked");
   }
 
-  @Test public void checkScrollToRecyclerViewItem_atSixty() {
-    int position = 60;
-    scrollTo(R.id.recycler, position);
+  @Test public void checkScrollToRecyclerViewItem() {
+    scrollTo(R.id.recycler, POSITION_AT_LAST_PAGE);
     assertDisplayed("Papaya");
   }
 
-  @Test public void checkClickNoButton_byPosition_andText_atSixty() {
-    int position = 60;
-    clickRecyclerViewItemChild(R.id.recycler, position, "No");
+  @Test public void checkClickItemChild_childItemByText() {
+    clickRecyclerViewItemChild(R.id.recycler, POSITION_AT_LAST_PAGE, "No");
     assertDisplayed("'no' has been clicked");
   }
 
-  @Test public void checkClickNoButton_byPosition_andResId_atSixty() {
-    int position = 60;
-    clickRecyclerViewItemChild(R.id.recycler, position, R.id.no);
+  @Test public void checkClickItemChild_childItemByPosition() {
+    clickRecyclerViewItemChild(R.id.recycler, POSITION_AT_LAST_PAGE, R.id.no);
     assertDisplayed("'no' has been clicked");
   }
 }
