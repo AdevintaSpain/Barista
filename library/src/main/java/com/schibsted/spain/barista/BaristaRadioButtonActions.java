6d2061e9c18a45ff5779c536b28b6f62b29aa2d3
Roc Boronat <roc@fewlaps.com>
2017-05-10 17:03:55 +0200

    Scroll before clicking the RadioButton to pass the tests

--- library/src/main/java/com/schibsted/spain/barista/BaristaRadioButtonActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaRadioButtonActions.java
@@ -5,6 +5,7 @@ import android.support.annotation.IdRes;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static com.schibsted.spain.barista.BaristaScrollActions.scrollTo;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithId;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithText;
 import static com.schibsted.spain.barista.custom.HelperMatchers.atPosition;
@@ -13,17 +14,20 @@ import static org.hamcrest.Matchers.allOf;
 public class BaristaRadioButtonActions {
 
   public static void clickRadioButtonItem(@IdRes int radioGroupId, @IdRes int itemToClickId) {
+    scrollTo(radioGroupId);
     onView(
         allOf(withParent(displayedWithId(radioGroupId)), displayedWithId(itemToClickId))).perform(
         click());
   }
 
   public static void clickRadioButtonItem(@IdRes int radioGroupId, String text) {
+    scrollTo(radioGroupId);
     onView(allOf(withParent(displayedWithId(radioGroupId)), displayedWithText(text))).perform(
         click());
   }
 
   public static void clickRadioButtonPosition(@IdRes int radioGroupId, int position) {
+    scrollTo(radioGroupId);
     onView(atPosition(position, withParent(displayedWithId(radioGroupId)))).perform(click());
   }
 }
