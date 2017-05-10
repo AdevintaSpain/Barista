23e0017fbc26d027dca88188b3dde9933b9899ac
Roc Boronat <roc@fewlaps.com>
2017-05-10 17:02:48 +0200

    Add a RadioGroup in a ScrollView that is really far away

--- sample/src/main/java/com/schibsted/spain/barista/sample/RadioButtonsActivity.java
+++ sample/src/main/java/com/schibsted/spain/barista/sample/RadioButtonsActivity.java
@@ -1,6 +1,7 @@
 package com.schibsted.spain.barista.sample;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,12 +14,18 @@ public class RadioButtonsActivity extends AppCompatActivity {
     setContentView(R.layout.activity_radiobuttons);
 
     RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
-    radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(RadioGroup radioGroup, int itemId) {
-        TextView textView = (TextView) findViewById(R.id.selected_item);
-        textView.setText("" + itemId);
-      }
-    });
+    RadioGroup reallyFarAwayRadioGroup = (RadioGroup) findViewById(R.id.radiogroup_really_far_away);
+
+    radioGroup.setOnCheckedChangeListener(new PutClickedIdOnTextView());
+    reallyFarAwayRadioGroup.setOnCheckedChangeListener(new PutClickedIdOnTextView());
+  }
+
+  private class PutClickedIdOnTextView implements RadioGroup.OnCheckedChangeListener {
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+      TextView textView = (TextView) findViewById(R.id.selected_item);
+      textView.setText("" + checkedId);
+    }
   }
 }
