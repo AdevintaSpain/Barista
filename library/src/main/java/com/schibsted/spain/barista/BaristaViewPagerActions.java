44f4028c2370404f5f963b3d649316af4bddca13
Roc Boronat <roc@fewlaps.com>
2017-05-10 17:56:26 +0200

    Implement the solution to the tests

--- library/src/main/java/com/schibsted/spain/barista/BaristaViewPagerActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaViewPagerActions.java
@@ -1,11 +1,15 @@
 package com.schibsted.spain.barista;
 
 import android.support.annotation.IdRes;
+import android.support.v4.view.ViewPager;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithId;
+import static org.hamcrest.core.AllOf.allOf;
 
 public class BaristaViewPagerActions {
 
@@ -15,5 +19,13 @@ public class BaristaViewPagerActions {
 
   public static void swipeViewPagerBack(@IdRes int id) {
     onView(displayedWithId(id)).perform(swipeRight());
+  }
+
+  public static void swipeViewPagerForward() {
+    onView(allOf(isDisplayed(), isAssignableFrom(ViewPager.class))).perform(swipeLeft());
+  }
+
+  public static void swipeViewPagerBack() {
+    onView(allOf(isDisplayed(), isAssignableFrom(ViewPager.class))).perform(swipeRight());
   }
 }
