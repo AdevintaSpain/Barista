dc7db28c416fe0ca5424bd776d5a9c22972fcb2b
Roc Boronat <roc@fewlaps.com>
2017-05-11 12:41:48 +0200

    Autoscroll on RadioButtonsAction (#55)
    * Add a RadioGroup in a ScrollView that is really far away
    
    * Write tests to reproduce clicking on scroll-needed RadioButtons
    
    * Scroll before clicking the RadioButton to pass the tests
    
    * Put the regular RadioGroup over the ScrollView to avoid losing clicks
    
    * Avoid crashing if the RadioButton is not inside a ScrollView

--- library/src/main/java/com/schibsted/spain/barista/BaristaRadioButtonActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaRadioButtonActions.java
@@ -1,10 +1,13 @@
 package com.schibsted.spain.barista;
 
 import android.support.annotation.IdRes;
+import android.support.test.espresso.PerformException;
+import android.util.Log;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static com.schibsted.spain.barista.BaristaScrollActions.scrollTo;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithId;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithText;
 import static com.schibsted.spain.barista.custom.HelperMatchers.atPosition;
@@ -13,17 +16,38 @@ import static org.hamcrest.Matchers.allOf;
 public class BaristaRadioButtonActions {
 
   public static void clickRadioButtonItem(@IdRes int radioGroupId, @IdRes int itemToClickId) {
-    onView(
-        allOf(withParent(displayedWithId(radioGroupId)), displayedWithId(itemToClickId))).perform(
-        click());
+    try {
+      scrollTo(radioGroupId);
+    } catch (PerformException exception) {
+      Log.d("Barista",
+          "The View's parent is not a ScrollView. Due to the power of Barista, you can ignore this error message");
+    } finally {
+      onView(
+          allOf(withParent(displayedWithId(radioGroupId)), displayedWithId(itemToClickId))).perform(
+          click());
+    }
   }
 
   public static void clickRadioButtonItem(@IdRes int radioGroupId, String text) {
-    onView(allOf(withParent(displayedWithId(radioGroupId)), displayedWithText(text))).perform(
-        click());
+    try {
+      scrollTo(radioGroupId);
+    } catch (PerformException exception) {
+      Log.d("Barista",
+          "The View's parent is not a ScrollView. Due to the power of Barista, you can ignore this error message");
+    } finally {
+      onView(allOf(withParent(displayedWithId(radioGroupId)), displayedWithText(text))).perform(
+          click());
+    }
   }
 
   public static void clickRadioButtonPosition(@IdRes int radioGroupId, int position) {
-    onView(atPosition(position, withParent(displayedWithId(radioGroupId)))).perform(click());
+    try {
+      scrollTo(radioGroupId);
+    } catch (PerformException exception) {
+      Log.d("Barista",
+          "The View's parent is not a ScrollView. Due to the power of Barista, you can ignore this error message");
+    } finally {
+      onView(atPosition(position, withParent(displayedWithId(radioGroupId)))).perform(click());
+    }
   }
 }
