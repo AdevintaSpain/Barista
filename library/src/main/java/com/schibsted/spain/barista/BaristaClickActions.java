67b4d4195875ebd931475ee670ba6e95f862a5f3
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-06-01 14:56:36 +0200

    Fixed BaristaClickActions to work with @String resources

--- library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
@@ -1,9 +1,12 @@
 package com.schibsted.spain.barista;
 
 import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.test.espresso.AmbiguousViewMatcherException;
 import android.support.test.espresso.PerformException;
 import android.support.test.espresso.action.ViewActions;
+import com.schibsted.spain.barista.androidresource.ResourceTypeChecker;
+import com.schibsted.spain.barista.exception.BaristaArgumentTypeException;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
@@ -15,17 +18,43 @@ import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWith
 
 public class BaristaClickActions {
 
-  public static void click(@IdRes int id) {
+  private static final ResourceTypeChecker RESOURCE_TYPE_CHECKER = new ResourceTypeChecker();
+
+  public static void click(int id) {
+    if (isStringResource(id)) {
+      clickByString(id);
+    } else if (isIdResource(id)) {
+      clickById(id);
+    } else {
+      throw new BaristaArgumentTypeException();
+    }
+  }
+
+  private static void clickById(@IdRes int id) {
     try {
-      scrollAndClickView(id);
+      scrollAndClickViewById(id);
     } catch (AmbiguousViewMatcherException multipleViewsMatched) {
       try {
-        scrollAndClickDisplayedView(id);
+        scrollAndClickDisplayedViewById(id);
       } catch (PerformException parentIsNotAnScrollView) {
-        clickDisplayedView(id);
+        clickDisplayedViewById(id);
       }
     } catch (PerformException parentIsNotAnScrollView) {
-      clickDisplayedView(id);
+      clickDisplayedViewById(id);
+    }
+  }
+
+  private static void clickByString(@StringRes int id) {
+    try {
+      scrollAndClickViewByString(id);
+    } catch (AmbiguousViewMatcherException multipleViewsMatched) {
+      try {
+        scrollAndClickDisplayedViewByString(id);
+      } catch (PerformException parentIsNotAnScrollView) {
+        clickDisplayedViewByString(id);
+      }
+    } catch (PerformException parentIsNotAnScrollView) {
+      clickDisplayedViewByString(id);
     }
   }
 
@@ -47,27 +76,46 @@ public class BaristaClickActions {
     pressBack();
   }
 
-  private static void scrollAndClickView(@IdRes int id) {
+  private static void scrollAndClickViewById(@IdRes int id) {
     onView(withId(id)).perform(scrollTo(), ViewActions.click());
+  }
+
+  private static void scrollAndClickViewByString(@StringRes int id) {
+    onView(withText(id)).perform(scrollTo(), ViewActions.click());
   }
 
   private static void scrollAndClickView(String text) {
     onView(withText(text)).perform(scrollTo(), ViewActions.click());
   }
 
-  private static void scrollAndClickDisplayedView(@IdRes int id) {
+  private static void scrollAndClickDisplayedViewById(@IdRes int id) {
     onView(displayedWithId(id)).perform(scrollTo(), ViewActions.click());
+  }
+  private static void scrollAndClickDisplayedViewByString(@StringRes int id) {
+    onView(displayedWithText(id)).perform(scrollTo(), ViewActions.click());
   }
 
   private static void scrollAndClickDisplayedView(String text) {
     onView(displayedWithText(text)).perform(scrollTo(), ViewActions.click());
   }
 
-  private static void clickDisplayedView(@IdRes int id) {
+  private static void clickDisplayedViewById(@IdRes int id) {
     onView(displayedWithId(id)).perform(ViewActions.click());
+  }
+
+  private static void clickDisplayedViewByString(@StringRes int id) {
+    onView(displayedWithText(id)).perform(ViewActions.click());
   }
 
   private static void clickDisplayedView(String text) {
     onView(displayedWithText(text)).perform(ViewActions.click());
+  }
+
+  private static boolean isIdResource(int id) {
+    return RESOURCE_TYPE_CHECKER.isIdResource(id);
+  }
+
+  private static boolean isStringResource(int id) {
+    return RESOURCE_TYPE_CHECKER.isStringResource(id);
   }
 }
