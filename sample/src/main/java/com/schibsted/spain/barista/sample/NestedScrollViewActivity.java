192ec787e31f3737f71f7770cfca1319cf2857f5
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-06-07 17:14:27 +0200

    Add scrollTo() action that works with NestedScrollView

---
+++ sample/src/main/java/com/schibsted/spain/barista/sample/NestedScrollViewActivity.java
@@ -0,0 +1,13 @@
+package com.schibsted.spain.barista.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NestedScrollViewActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_nested_scroll);
+  }
+}
