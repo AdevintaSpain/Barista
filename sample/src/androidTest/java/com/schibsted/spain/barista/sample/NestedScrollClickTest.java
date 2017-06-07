68da7762a18b6f1ae9dd0182234b3afc7bceafdd
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-06-07 17:24:31 +0200

    Add test to click() on NestedScrollView

---
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/NestedScrollClickTest.java
@@ -0,0 +1,29 @@
+package com.schibsted.spain.barista.sample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.schibsted.spain.barista.BaristaAssertions.assertDisplayed;
+import static com.schibsted.spain.barista.BaristaClickActions.click;
+
+@RunWith(AndroidJUnit4.class)
+public class NestedScrollClickTest {
+
+  @Rule
+  public ActivityTestRule<NestedScrollViewActivity> activityRule = new ActivityTestRule<>(NestedScrollViewActivity.class);
+
+  @Test
+  public void checkClickScrollsIfNeeded_byId() {
+    click(R.id.really_far_away_button);
+    assertDisplayed("Hi! I'm the second screen!");
+  }
+
+  @Test
+  public void checkClickScrollsIfNeeded_byText() {
+    click("Really far away button");
+    assertDisplayed("Hi! I'm the second screen!");
+  }
+}
