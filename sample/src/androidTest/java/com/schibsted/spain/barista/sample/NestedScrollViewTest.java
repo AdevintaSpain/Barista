6e55c6da64dc0f57e34c5f4c70a018cf795e2c70
Bernat Borrás Paronella <alorma@users.noreply.github.com>
2017-06-08 16:02:08 +0200

    Add scrollTo() action that works with NestedScrollView (#96)
    * Add scrollTo() action that works with NestedScrollView
    
    * Add test to click() on NestedScrollView
    
    * Rename BaristaScrollToAction
    
    * Remove desing library from Barista library module
    
    * Fix @rocboronat comments from PR

---
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/NestedScrollViewTest.java
@@ -0,0 +1,40 @@
+package com.schibsted.spain.barista.sample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.schibsted.spain.barista.BaristaAssertions.assertDisplayed;
+import static com.schibsted.spain.barista.BaristaAssertions.assertNotDisplayed;
+import static com.schibsted.spain.barista.BaristaScrollActions.scrollTo;
+
+@RunWith(AndroidJUnit4.class)
+public class NestedScrollViewTest {
+
+  @Rule
+  public ActivityTestRule<NestedScrollViewActivity> activityRule = new ActivityTestRule<>(NestedScrollViewActivity.class);
+
+  @Test
+  public void checkScrollByText() {
+    assertDisplayed("Hi! I'm the first screen!");
+    assertNotDisplayed("Really far away button");
+
+    scrollTo("Really far away button");
+
+    assertNotDisplayed("Hi! I'm the first screen!");
+    assertDisplayed("Really far away button");
+  }
+
+  @Test
+  public void checkScrollById() {
+    assertDisplayed("Hi! I'm the first screen!");
+    assertNotDisplayed("Really far away button");
+
+    scrollTo(R.id.really_far_away_button);
+
+    assertNotDisplayed("Hi! I'm the first screen!");
+    assertDisplayed("Really far away button");
+  }
+}
