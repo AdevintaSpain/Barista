5377e8aae358ad23e90c89078f435452cad2e84a
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-08-26 17:40:41 +0200

    Add instrumentation test to check BitmapComparator

---
+++ library/src/androidTest/java/com/schibsted/spain/barista/util/BitmapComparatorTest.kt
@@ -0,0 +1,31 @@
+package com.schibsted.spain.barista.util
+
+import android.graphics.BitmapFactory
+import android.support.test.InstrumentationRegistry
+import com.schibsted.spain.barista.test.R
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+class BitmapComparatorTest {
+
+  @Test
+  fun returnTrueWhenComparingTheSameDrawable() {
+    val aBitmap = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().resources, R.mipmap.ic_barista)
+    val theSameBitmap = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().resources, R.mipmap.ic_barista)
+
+    val result = BitmapComparator.compare(aBitmap, theSameBitmap)
+
+    assertTrue(result)
+  }
+
+  @Test
+  fun returnFalseWhenComparingDifferentDrawables() {
+    val aBitmap = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().resources, R.mipmap.ic_barista)
+    val aDifferentBitmap = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().resources, R.mipmap.ic_launcher)
+
+    val result = BitmapComparator.compare(aBitmap, aDifferentBitmap)
+
+    assertFalse(result)
+  }
+}
