cea10dbed52bdcbe8daa913d3fdf712278ddfa3e
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-08-30 12:45:18 +0200

    Migrate ClearDatabaseRule to Kotlin (#125)
    * Added mockito dependency
    
    With the inline Mock Maker to allow mocking kotlin files
    
    * Migrate ClearDatabaseRule to Kotlin
    
    I refactored the responsabilities of the rule and the DatabaseOperations file. The latter is now a simple collaborator to abstract from Android's SQLite database, instead of doing all the work.
    The rule logic is now on the rule itself.
    
    I added unit tests for the behavior of the rule. It wasn't really possible before because DatabaseOperations was made of static methods.

---
+++ library/src/test/java/com/schibsted/spain/barista/cleardata/ClearDatabaseRuleTest.java
@@ -0,0 +1,107 @@
+package com.schibsted.spain.barista.cleardata;
+
+import android.database.sqlite.SQLiteDatabase;
+import java.io.File;
+import java.util.List;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@RunWith(JUnitParamsRunner.class)
+public class ClearDatabaseRuleTest {
+
+  private static final File DB_1_FILE = new File("db1.db");
+  private static final File DB_2_FILE = new File("db2.db");
+  private static final SQLiteDatabase DB_1 = mock(SQLiteDatabase.class);
+  private static final SQLiteDatabase DB_2 = mock(SQLiteDatabase.class);
+  private static final List<String> DB_1_TABLES = asList("db1_table1", "db1_table2");
+  private static final List<String> DB_2_TABLES = asList("db2_table1", "db2_table2");
+
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Mock
+  DatabaseOperations operations;
+  @Mock
+  Statement dummyStatement;
+  @Mock
+  Description dummyDescription;
+
+  @Before
+  public void setUp() throws Exception {
+    given(operations.openDatabase(DB_1_FILE)).willReturn(DB_1);
+    given(operations.openDatabase(DB_2_FILE)).willReturn(DB_2);
+    given(operations.getTableNames(DB_1)).willReturn(DB_1_TABLES);
+    given(operations.getTableNames(DB_2)).willReturn(DB_2_TABLES);
+  }
+
+  @Test
+  public void deleteMultipleDatabases() throws Throwable {
+    given(operations.getAllDatabaseFiles()).willReturn(asList(DB_1_FILE, DB_2_FILE));
+
+    executeRule();
+
+    verify(operations, atLeastOnce()).deleteTableContent(eq(DB_1), anyString());
+    verify(operations, atLeastOnce()).deleteTableContent(eq(DB_2), anyString());
+  }
+
+  @Test
+  public void deleteAllTablesFromDatabase() throws Throwable {
+    given(operations.getAllDatabaseFiles()).willReturn(asList(DB_1_FILE, DB_2_FILE));
+
+    executeRule();
+
+    verify(operations, atLeastOnce()).deleteTableContent(DB_1, DB_1_TABLES.get(0));
+    verify(operations, atLeastOnce()).deleteTableContent(DB_1, DB_1_TABLES.get(1));
+    verify(operations, atLeastOnce()).deleteTableContent(DB_2, DB_2_TABLES.get(0));
+    verify(operations, atLeastOnce()).deleteTableContent(DB_2, DB_2_TABLES.get(1));
+  }
+
+  @Test
+  @Parameters(method = "unwantedSuffixesParameters")
+  public void doesNotDeleteFilesWithUnwantedSuffixes(String suffix) throws Throwable {
+    File unwantedFile = new File("unwanted.db" + suffix);
+    given(operations.getAllDatabaseFiles()).willReturn(singletonList(unwantedFile));
+
+    executeRule();
+
+    verify(operations, never()).deleteTableContent(any(SQLiteDatabase.class), anyString());
+  }
+
+  private String[] unwantedSuffixesParameters() {
+    // Don't understand this method? Check out https://github.com/Pragmatists/JUnitParams
+    return ClearDatabaseRule.UNWANTED_FILENAME_SUFFIXES;
+  }
+
+  @Test
+  public void closesDatabase() throws Throwable {
+    given(operations.getAllDatabaseFiles()).willReturn(singletonList(DB_1_FILE));
+
+    executeRule();
+
+    verify(DB_1, atLeastOnce()).close();
+  }
+
+  private void executeRule() throws Throwable {
+    new ClearDatabaseRule(operations).apply(dummyStatement, dummyDescription).evaluate();
+  }
+}
