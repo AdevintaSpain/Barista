064959b0135d63f6ef6fe84c364178e7ec943b0c
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-09-27 17:12:18 +0200

    Fix click with scroll and mutliple views

--- library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
@@ -1,18 +1,27 @@
 package com.schibsted.spain.barista;
 
 import android.support.test.espresso.AmbiguousViewMatcherException;
-import android.support.test.espresso.PerformException;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.action.ViewActions;
+import android.support.v4.widget.NestedScrollView;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import com.schibsted.spain.barista.androidresource.ResourceTypeChecker;
 import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.schibsted.spain.barista.custom.NestedEnabledScrollToAction.scrollTo;
 import static com.schibsted.spain.barista.custom.DisplayedMatchers.displayedAnd;
+import static com.schibsted.spain.barista.custom.NestedEnabledScrollToAction.scrollTo;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.AnyOf.anyOf;
 
 public class BaristaClickActions {
 
@@ -32,15 +41,13 @@ public class BaristaClickActions {
 
   public static void click(Matcher<View> viewMatcher) {
     try {
-      scrollAndClickView(viewMatcher);
-    } catch (AmbiguousViewMatcherException multipleViewsMatched) {
-      try {
-        scrollAndClickDisplayedView(viewMatcher);
-      } catch (PerformException parentIsNotAnScrollView) {
-        clickDisplayedView(viewMatcher);
-      }
-    } catch (PerformException parentIsNotAnScrollView) {
       clickDisplayedView(viewMatcher);
+    } catch (NoMatchingViewException noMatchingError) {
+      try {
+        scrollAndClickView(viewMatcher);
+      } catch (AmbiguousViewMatcherException multipleViewsError) {
+        scrollAndClickDisplayedView(viewMatcher);
+      }
     }
   }
 
@@ -53,7 +60,18 @@ public class BaristaClickActions {
   }
 
   private static void scrollAndClickDisplayedView(Matcher<View> viewMatcher) {
-    onView(displayedAnd(viewMatcher)).perform(scrollTo(), ViewActions.click());
+    onView(allOf(
+        viewMatcher,
+        isDescendantOfA(allOf(
+            isDisplayed(),
+            anyOf(
+                isAssignableFrom(ScrollView.class),
+                isAssignableFrom(HorizontalScrollView.class),
+                isAssignableFrom(ListView.class),
+                isAssignableFrom(NestedScrollView.class)
+            )
+        ))
+    )).perform(scrollTo(), ViewActions.click());
   }
 
   private static void clickDisplayedView(Matcher<View> viewMatcher) {
