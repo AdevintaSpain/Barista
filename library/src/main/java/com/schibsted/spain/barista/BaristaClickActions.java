702b2b2deaec4f7af63e826f0aca67fc3070ef7a
Etienne Wojahn <etienne.wojahn@me.com>
2017-10-20 10:48:46 +0200

    Added LongClickActions to BaristaClickActions

--- library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaClickActions.java
@@ -2,13 +2,16 @@ package com.schibsted.spain.barista;
 
 import android.support.test.espresso.AmbiguousViewMatcherException;
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.v4.widget.NestedScrollView;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ListView;
 import android.widget.ScrollView;
+
 import com.schibsted.spain.barista.androidresource.ResourceTypeChecker;
+
 import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -25,64 +28,82 @@ import static org.hamcrest.core.AnyOf.anyOf;
 
 public class BaristaClickActions {
 
-  private static final ResourceTypeChecker RESOURCE_TYPE_CHECKER = new ResourceTypeChecker();
-
-  public static void click(int id) {
-    if (isIdResource(id)) {
-      click(withId(id));
-    } else if (isStringResource(id)) {
-      click(withText(id));
+    private static final ResourceTypeChecker RESOURCE_TYPE_CHECKER = new ResourceTypeChecker();
+    
+    // SingleClick Actions
+    public static void click(int id) {
+        performClick(id, ViewActions.click());
     }
-  }
 
-  public static void click(String text) {
-    click(withText(text));
-  }
-
-  public static void click(Matcher<View> viewMatcher) {
-    try {
-      clickDisplayedView(viewMatcher);
-    } catch (NoMatchingViewException noMatchingError) {
-      try {
-        scrollAndClickView(viewMatcher);
-      } catch (AmbiguousViewMatcherException multipleViewsError) {
-        scrollAndClickDisplayedView(viewMatcher);
-      }
+    public static void click(String text) {
+        click(withText(text), ViewActions.click());
     }
-  }
 
-  public static void clickBack() {
-    pressBack();
-  }
+    // LongClick Actions
+    public static void longClick(int id) {
+        performClick(id, ViewActions.longClick());
+    }
 
-  private static void scrollAndClickView(Matcher<View> viewMatcher) {
-    onView(viewMatcher).perform(scrollTo(), ViewActions.click());
-  }
+    public static void longClick(String text) {
+        click(withText(text), ViewActions.longClick());
+    }
 
-  private static void scrollAndClickDisplayedView(Matcher<View> viewMatcher) {
-    onView(allOf(
-        viewMatcher,
-        isDescendantOfA(allOf(
-            isDisplayed(),
-            anyOf(
-                isAssignableFrom(ScrollView.class),
-                isAssignableFrom(HorizontalScrollView.class),
-                isAssignableFrom(ListView.class),
-                isAssignableFrom(NestedScrollView.class)
-            )
-        ))
-    )).perform(scrollTo(), ViewActions.click());
-  }
+    private static void performClick(int id, ViewAction clickType) {
+        if (isIdResource(id)) {
+            click(withId(id), clickType);
+        } else if (isStringResource(id)) {
+            click(withText(id), clickType);
+        }
+    }
 
-  private static void clickDisplayedView(Matcher<View> viewMatcher) {
-    onView(displayedAnd(viewMatcher)).perform(ViewActions.click());
-  }
+    public static void click(Matcher<View> viewMatcher, ViewAction clickType) {
+        try {
+            clickDisplayedView(viewMatcher, clickType);
+        } catch (NoMatchingViewException noMatchingError) {
+            try {
+                scrollAndClickView(viewMatcher, clickType);
+            } catch (AmbiguousViewMatcherException multipleViewsError) {
+                scrollAndClickDisplayedView(viewMatcher, clickType);
+            }
+        }
+    }
 
-  private static boolean isIdResource(int id) {
-    return RESOURCE_TYPE_CHECKER.isIdResource(id);
-  }
+    public static void clickBack() {
+        pressBack();
+    }
 
-  private static boolean isStringResource(int id) {
-    return RESOURCE_TYPE_CHECKER.isStringResource(id);
-  }
+    private static void scrollAndClickView(Matcher<View> viewMatcher, ViewAction clickType) {
+        onView(viewMatcher).perform(scrollTo(), clickType);
+    }
+
+    private static void scrollAndClickDisplayedView(Matcher<View> viewMatcher, ViewAction clickType) {
+        onView(allOf(
+                viewMatcher,
+                isDescendantOfA(allOf(
+                        isDisplayed(),
+                        anyOf(
+                                isAssignableFrom(ScrollView.class),
+                                isAssignableFrom(HorizontalScrollView.class),
+                                isAssignableFrom(ListView.class),
+                                isAssignableFrom(NestedScrollView.class)
+                        )
+                ))
+        )).perform(scrollTo(), clickType);
+    }
+
+    private static void clickDisplayedView(Matcher<View> viewMatcher, ViewAction clickType) {
+        onView(displayedAnd(viewMatcher)).perform(clickType);
+    }
+
+    private static boolean isIdResource(int id) {
+        return RESOURCE_TYPE_CHECKER.isIdResource(id);
+    }
+
+    private static boolean isStringResource(int id) {
+        return RESOURCE_TYPE_CHECKER.isStringResource(id);
+    }
+
+
+
+
 }
