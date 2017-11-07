6d56ca55282336f18e69cbde39ecd492308d455e
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-11-07 11:15:17 +0100

    Composition for displayed matchers.

--- library/src/main/java/com/schibsted/spain/barista/BaristaAutoCompleteTextViewActions.kt
+++ library/src/main/java/com/schibsted/spain/barista/BaristaAutoCompleteTextViewActions.kt
@@ -2,16 +2,16 @@ package com.schibsted.spain.barista
 
 import android.support.annotation.IdRes
 import android.support.test.espresso.Espresso.onView
-import com.schibsted.spain.barista.custom.AutocompleteViewActions.replaceAutocomplete
-import com.schibsted.spain.barista.custom.DisplayedMatchers.displayedWithId
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import com.schibsted.spain.barista.BaristaScrollActions.safelyScrollTo
+import com.schibsted.spain.barista.custom.AutocompleteViewActions.replaceAutocomplete
+import com.schibsted.spain.barista.custom.DisplayedMatchers.displayedAnd
 
 object BaristaAutoCompleteTextViewActions {
 
     @JvmStatic
     fun writeToAutoCompleteTextView(@IdRes id: Int, text: String) {
         safelyScrollTo(id)
-
-        onView(displayedWithId(id)).perform(replaceAutocomplete(text))
+        onView(displayedAnd(withId(id))).perform(replaceAutocomplete(text))
     }
 }
