e9e4fbf7df37e51b30633ac85f47c6615d912f4c
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-11-07 12:55:22 +0100

    Simplifly internals of ListViewActions
    We were implementing the `clickListItemForMultipleListsOnScreen` wrong. It's for a very specific use-case in one of our internal apps, and it doesn't really do what it says.
    Please lord forgive us, we were new to Espresso.

--- library/src/main/java/com/schibsted/spain/barista/BaristaListViewActions.kt
+++ library/src/main/java/com/schibsted/spain/barista/BaristaListViewActions.kt
@@ -3,9 +3,6 @@ package com.schibsted.spain.barista
 import android.support.annotation.IdRes
 import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.NoMatchingViewException
-import android.support.test.espresso.matcher.ViewMatchers.*
-import android.view.View
-import android.widget.AdapterView
 import com.schibsted.spain.barista.custom.PerformClickAction.clickUsingPerformClick
 import com.schibsted.spain.barista.internal.failurehandler.SpyFailureHandler
 import com.schibsted.spain.barista.internal.failurehandler.withFailureHandler
@@ -17,7 +14,7 @@ object BaristaListViewActions {
     fun clickListViewItem(@IdRes listViewId: Int, vararg positions: Int) {
         check(positions.isNotEmpty(), { "Positions cannot be empty" })
         positions.forEach {
-            clickItem(listViewId, it)
+            clickItem(it)
         }
     }
 
@@ -25,67 +22,36 @@ object BaristaListViewActions {
     fun clickListViewItem(@IdRes listViewId: Int, modelClass: Class<*>, vararg positions: Int) {
         check(positions.isNotEmpty(), { "Positions cannot be empty" })
         positions.forEach {
-            clickItem(listViewId, it, modelClass)
+            clickItem(it, modelClass)
         }
     }
 
-    private fun clickItem(@IdRes listViewId: Int, position: Int) {
+    private fun clickItem(position: Int) {
         val spyHandler = SpyFailureHandler()
         try {
-            withFailureHandler(spyHandler) { clickListItemForSingleListOnScreen(position) }
+            withFailureHandler(spyHandler) { clickListItemByPosition(position) }
         } catch (e: NoMatchingViewException) {
-            try {
-                withFailureHandler(spyHandler) { clickListItemForMultipleListsOnScreen(listViewId, position) }
-            } catch (e: Throwable) {
-                spyHandler.resendLastError("Could not click on list item $position")
-            }
+            spyHandler.resendLastError("Could not click on list item $position")
         }
     }
 
-    private fun clickItem(@IdRes listViewId: Int, position: Int, modelClass: Class<*>) {
+    private fun clickItem(position: Int, modelClass: Class<*>) {
         val spyHandler = SpyFailureHandler()
         try {
-            withFailureHandler(spyHandler) { clickListItemForMultipleListsOnScreen(listViewId, position, modelClass) }
-        } catch (e: NoMatchingViewException) {
-            try {
-                withFailureHandler(spyHandler) { clickListItemForSingleListOnScreen(position, modelClass) }
-            } catch (e: Throwable) {
-                spyHandler.resendLastError("Could not click on list item $position of type ${modelClass.simpleName}")
-            }
+            withFailureHandler(spyHandler) { clickListItemByPositionAndClass(position, modelClass) }
+        } catch (e: Throwable) {
+            spyHandler.resendLastError("Could not click on list item $position of type ${modelClass.simpleName}")
         }
-
     }
 
-    private fun clickListItemForMultipleListsOnScreen(@IdRes listViewId: Int, position: Int) {
-        // This method only seems to work when there are multiple ListViews in the same activity
-        onData(anything()).inAdapterView(
-                allOf<View>(
-                        isAssignableFrom(AdapterView::class.java),
-                        isDescendantOfA(withId(listViewId)),
-                        isDisplayed()))
-                .atPosition(position)
-                .perform(clickUsingPerformClick())
-    }
-
-    private fun clickListItemForMultipleListsOnScreen(@IdRes listViewId: Int, position: Int, modelClass: Class<*>) {
-        // This method only seems to work when there are multiple ListViews in the same activity
-        onData(`is`(instanceOf(modelClass))).inAdapterView(
-                allOf<View>(
-                        isAssignableFrom(AdapterView::class.java),
-                        isDescendantOfA(withId(listViewId)),
-                        isDisplayed()))
-                .atPosition(position)
-                .perform(clickUsingPerformClick())
-    }
-
-    private fun clickListItemForSingleListOnScreen(position: Int) {
+    private fun clickListItemByPosition(position: Int) {
         // This method only seems to work when there is just one ListView in the same activity
         onData(anything())
                 .atPosition(position)
                 .perform(clickUsingPerformClick())
     }
 
-    private fun clickListItemForSingleListOnScreen(position: Int, modelClass: Class<*>) {
+    private fun clickListItemByPositionAndClass(position: Int, modelClass: Class<*>) {
         // This method only seems to work when there is just one ListView in the same activity
         onData(`is`(instanceOf(modelClass)))
                 .atPosition(position)
