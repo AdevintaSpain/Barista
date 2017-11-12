d3bcaeff6e592d23fbc29a8cfd6cd40f51c70a54
Esteve Aguilera <esteve@fewlaps.com>
2017-11-12 14:54:10 +0100

    Add two EditTexts to test the new focus assertions
    To be able to test the hasFocus and the hasNotFocus assertions, two EditText has been added on the SomeViewsWithDifferentVisibilitesActivity
    
    When launching the Activity the requestFocus is applied to the edittext_with_focus view to ensure it gets the focus

--- sample/src/main/java/com/schibsted/spain/barista/sample/SomeViewsWithDifferentVisibilitesActivity.java
+++ sample/src/main/java/com/schibsted/spain/barista/sample/SomeViewsWithDifferentVisibilitesActivity.java
@@ -9,5 +9,7 @@ public class SomeViewsWithDifferentVisibilitesActivity extends AppCompatActivity
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.edittext_with_focus).requestFocus();
   }
 }
