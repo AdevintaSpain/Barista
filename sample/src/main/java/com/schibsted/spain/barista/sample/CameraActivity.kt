3cc1a11c70aa783e5b334c1659c3411cbc258caa
Roc Boronat <roc@fewlaps.com>
2017-11-14 01:55:26 +0100

    Migrate the Activity to Kotlin

---
+++ sample/src/main/java/com/schibsted/spain/barista/sample/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.schibsted.spain.barista.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
