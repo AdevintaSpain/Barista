031516ae2899f1fa23fa570cf547b3d49d5c83c0
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-11-14 08:52:47 +0100

    Merge RecyclerView and ListView actions into ListActions (#146)
    * Create BaristaListActions to perform actions on ListView and RecyclerView
    
    Unlike BaristaListViewActions and BaristaRecylerViewActions, this new class will handle both list components without distinction to the user.
    Also id is now optional. If there are more than one listview and/or recyclerview displayed it will be mandatory.
    
    I created a ListsActivity to create layouts with different lists combinations dynamically from the test. This allows us to test multiple scenarios without having a different activity for each one.
    
    * Add scroll action to BaristaListActions
    
    * Extract common list action logic to performMagicAction function
    
    The logic is always the same: verify a single match, try performing on a recycler, try performing on a listview
    Also, the recycler and listview actions logic is the same respectively, so I also extracted them to common functions.
    
    * Add actions to click on list childs
    
    * Replace methods on RecyclerView with ViewPager test
    
    * Delete old ListView and RecyclerView actions and tests
    
    * Checkstyle violation
    
    * Fix parameter ordering
    
    * Change package visibility for findbugs
    
    * Fix lint violation
    
    * Fix PMD violations. I hate my life

---
+++ library/src/main/java/com/schibsted/spain/barista/action/BaristaListActions.kt
@@ -0,0 +1,109 @@
+package com.schibsted.spain.barista.action
+
+import android.support.annotation.IdRes
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.AmbiguousViewMatcherException
+import android.support.test.espresso.Espresso.onData
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.FailureHandler
+import android.support.test.espresso.NoMatchingViewException
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.action.ViewActions.scrollTo
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.widget.ListView
+import com.schibsted.spain.barista.internal.failurehandler.SpyFailureHandler
+import com.schibsted.spain.barista.internal.failurehandler.withFailureHandler
+import com.schibsted.spain.barista.internal.viewaction.ClickChildAction.clickChildWithId
+import com.schibsted.spain.barista.internal.viewaction.PerformClickAction.clickUsingPerformClick
+import org.hamcrest.Matchers.*
+
+object BaristaListActions {
+
+    @JvmStatic
+    @JvmOverloads
+    fun clickListItem(@IdRes id: Int? = null, position: Int) {
+        performMagicAction(id, position,
+                recyclerAction = actionOnItemAtPosition<ViewHolder>(position, clickUsingPerformClick()),
+                listViewAction = clickUsingPerformClick()
+        )
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun scrollListToPosition(@IdRes id: Int? = null, position: Int) {
+        performMagicAction(id, position,
+                recyclerAction = scrollToPosition<ViewHolder>(position),
+                listViewAction = scrollTo()
+        )
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun clickListItemChild(@IdRes id: Int? = null, position: Int, @IdRes childId: Int) {
+        performMagicAction(id, position,
+                recyclerAction = actionOnItemAtPosition<ViewHolder>(position, clickChildWithId(childId)),
+                listViewAction = clickChildWithId(childId)
+        )
+    }
+
+    private fun performMagicAction(@IdRes id: Int?, position: Int, recyclerAction: ViewAction, listViewAction: ViewAction) {
+        verifyOneSingleMatch(id)
+
+        val spyFailureHandler = SpyFailureHandler()
+        try {
+            performOnRecycler(id, recyclerAction, spyFailureHandler)
+        } catch (noRecyclerMatching: NoMatchingViewException) {
+            performOnListView(id, position, listViewAction, spyFailureHandler)
+        }
+    }
+
+    private fun verifyOneSingleMatch(@IdRes id: Int?) {
+        val spyFailureHandler = SpyFailureHandler()
+        try {
+            onView(anyOf(findListViewMatcher(id), findRecyclerMatcher(id)))
+                    .withFailureHandler(spyFailureHandler)
+                    .check(matches(isDisplayed()))
+        } catch (multipleViews: AmbiguousViewMatcherException) {
+            spyFailureHandler.resendLastError("There are multiple ListView or RecyclerView in the hierarchy. You must specify an id parameter using clickListItem(id, position)")
+        } catch (noneFound: NoMatchingViewException) {
+            val message = if (id == null) {
+                "No ListView or RecyclerView found in the hierarchy"
+            } else {
+                "No ListView or RecyclerView with id ${resourceName(id)} was found in the hierarchy. Did you use a wrong id?"
+            }
+            spyFailureHandler.resendLastError(message)
+        }
+    }
+
+    private fun performOnRecycler(@IdRes id: Int?, recyclerAction: ViewAction, failureHandler: FailureHandler) {
+        onView(findRecyclerMatcher(id))
+                .withFailureHandler(failureHandler)
+                .perform(recyclerAction)
+    }
+
+    private fun performOnListView(@IdRes id: Int?, position: Int, listViewAction: ViewAction, failureHandler: FailureHandler) {
+        withFailureHandler(failureHandler) {
+            onData(anything())
+                    .inAdapterView(findListViewMatcher(id))
+                    .atPosition(position)
+                    .perform(listViewAction)
+        }
+    }
+
+    private fun findRecyclerMatcher(@IdRes id: Int?) = when (id) {
+        null -> allOf(isDisplayed(), isAssignableFrom(RecyclerView::class.java))
+        else -> allOf(isDisplayed(), isAssignableFrom(RecyclerView::class.java), withId(id))
+    }
+
+    private fun findListViewMatcher(@IdRes id: Int?) = when (id) {
+        null -> allOf(isDisplayed(), isAssignableFrom(ListView::class.java))
+        else -> allOf(isDisplayed(), isAssignableFrom(ListView::class.java), withId(id))
+    }
+
+    private fun resourceName(resId: Int) = InstrumentationRegistry.getTargetContext().resources.getResourceName(resId)
+}
--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/RecyclerViewInsideViewPagerTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/RecyclerViewInsideViewPagerTest.java
@@ -7,10 +7,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.schibsted.spain.barista.action.BaristaListActions.clickListItem;
+import static com.schibsted.spain.barista.action.BaristaListActions.clickListItemChild;
+import static com.schibsted.spain.barista.action.BaristaListActions.scrollListToPosition;
 import static com.schibsted.spain.barista.assertion.BaristaAssertions.assertDisplayed;
-import static com.schibsted.spain.barista.action.BaristaRecyclerViewActions.clickRecyclerViewItem;
-import static com.schibsted.spain.barista.action.BaristaRecyclerViewActions.clickRecyclerViewItemChild;
-import static com.schibsted.spain.barista.action.BaristaRecyclerViewActions.scrollTo;
 
 @RunWith(AndroidJUnit4.class)
 public class RecyclerViewInsideViewPagerTest {
@@ -26,25 +26,19 @@ public class RecyclerViewInsideViewPagerTest {
 
   @Test
   public void checkClickRecyclerViewItem() {
-    clickRecyclerViewItem(R.id.recycler, POSITION_AT_LAST_PAGE);
+    clickListItem(R.id.recycler, POSITION_AT_LAST_PAGE);
     assertDisplayed("Papaya has been clicked");
   }
 
   @Test
   public void checkScrollToRecyclerViewItem() {
-    scrollTo(R.id.recycler, POSITION_AT_LAST_PAGE);
+    scrollListToPosition(R.id.recycler, POSITION_AT_LAST_PAGE);
     assertDisplayed("Papaya");
   }
 
   @Test
-  public void checkClickItemChild_childItemByText() {
-    clickRecyclerViewItemChild(R.id.recycler, POSITION_AT_LAST_PAGE, "No");
-    assertDisplayed("'no' has been clicked");
-  }
-
-  @Test
   public void checkClickItemChild_childItemByPosition() {
-    clickRecyclerViewItemChild(R.id.recycler, POSITION_AT_LAST_PAGE, R.id.no);
+    clickListItemChild(R.id.recycler, POSITION_AT_LAST_PAGE, R.id.no);
     assertDisplayed("'no' has been clicked");
   }
 }
