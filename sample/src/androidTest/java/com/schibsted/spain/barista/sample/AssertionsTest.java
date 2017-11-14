cf9eac8919832e6eee62c7ae184aa63b252a912d
Roc Boronat <roc@fewlaps.com>
2017-11-14 03:42:01 +0100

    Give simpler names to views

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/AssertionsTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/AssertionsTest.java
@@ -272,17 +272,17 @@ public class AssertionsTest {
 
   @Test
   public void checkDrawable_withId_withSameDrawable() throws Exception {
-    assertDrawable(R.id.image_view_with_image, R.drawable.ic_barista);
+    assertDrawable(R.id.image_view, R.drawable.ic_barista);
   }
 
   @Test(expected = AssertionFailedError.class)
   public void checkDrawable_withId_withDifferentDrawable() throws Exception {
-    assertDrawable(R.id.image_view_with_image, R.drawable.ic_action_menu);
+    assertDrawable(R.id.image_view, R.drawable.ic_action_menu);
   }
 
   @Test
   public void checkDrawable_withoutId_withDrawable() throws Exception {
-    assertDrawable(R.id.image_view_with_image);
+    assertDrawable(R.id.image_view);
   }
 
   @Test
