09ceb5206a16c5c02c9f4b3804d9d34eb7eb5976
Roc Boronat <roc@fewlaps.com>
2017-11-14 02:48:55 +0100

    Finish the implementation of the CameraAcitivity

--- sample/src/main/java/com/schibsted/spain/barista/sample/CameraActivity.kt
+++ sample/src/main/java/com/schibsted/spain/barista/sample/CameraActivity.kt
@@ -1,12 +1,44 @@
 package com.schibsted.spain.barista.sample
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_camera.image
+import kotlinx.android.synthetic.main.activity_camera.take_picture
+import java.io.File
 
 class CameraActivity : AppCompatActivity() {
+
+    companion object {
+        val TAKE_PICTURE_REQUEST_CODE = 42
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        take_picture.setOnClickListener {
+            val uri = getPictureUri()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, uri)
+            intent.putExtra("return-data", false)
+            startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == TAKE_PICTURE_REQUEST_CODE) {
+            Glide.with(this).load(getPictureUri()).into(image)
+        }
+    }
+
+    private fun getPictureUri(): Uri {
+        val path = applicationContext.cacheDir.path + "/test.jpg"
+        return FileProvider.getUriForFile(this,
+                applicationContext.packageName + ".barista.sample.provider",
+                File(path))
     }
 }
