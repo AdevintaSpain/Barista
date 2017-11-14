48e08d58045f5412ef35ae86aa542b99c29a99e0
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-11-14 14:48:58 +0100

    Create FlakyTestRule independent from ActivityTestRule (#160)
    * Create FlakyTestRule independent from ActivityTestRule
    
    Solves https://github.com/SchibstedSpain/Barista/issues/131
    
    I updated the test to use a combination of ActivityTestRule and FlakyTestRule in a RuleChain.
    
    * Remove FlakyActivityTestRule from tests
    
    We had a couple of tests using it without any apparent reason.
    
    * Update javadoc of AllowFlaky and Repeat annotations
    
    * Update BaristaRule to use FlakyTestRule instead of the deprecated FlakyActivityTestRule

--- library/src/main/java/com/schibsted/spain/barista/rule/flaky/AllowFlaky.java
+++ library/src/main/java/com/schibsted/spain/barista/rule/flaky/AllowFlaky.java
@@ -7,10 +7,10 @@ import java.lang.annotation.Target;
 /**
  * It let's flaky tests pass.
  * <br>
- * This annotation together with {@link FlakyActivityTestRule} repeats the annotated test several times, and makes the test pass if any of
+ * This annotation together with {@link FlakyTestRule} repeats the annotated test several times, and makes the test pass if any of
  * the executions passed. It's the opposite of {@link Repeat}.
  * <br>
- * You can use {@link FlakyActivityTestRule#allowFlakyAttemptsByDefault(int)} instead.
+ * You can use {@link FlakyTestRule#allowFlakyAttemptsByDefault(int)} to avoid repeating the annotation on every test method.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({
