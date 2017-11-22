100d36848d83de7c59e0e76bd8aaa2d7f38e2d50
Roc Boronat <roc@fewlaps.com>
2017-11-22 10:07:08 +0100

    Add blank separators to showcase the given, when and test parts of the tests

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/ScrollsTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/ScrollsTest.java
@@ -25,56 +25,72 @@ public class ScrollsTest {
   @Test
   public void scrollsInsideScrollable_byId() {
     assertTopVisible();
+
     scrollTo(R.id.really_far_away_button);
+
     assertBottomVisible();
   }
 
   @Test
   public void scrollsInsideScrollable_byHardcodedText() {
     assertTopVisible();
+
     scrollTo("Really far away button");
+
     assertBottomVisible();
   }
 
   @Test
   public void scrollsInsideScrollable_byTextResource() {
     assertTopVisible();
+
     scrollTo(R.string.really_far_away_button);
+
     assertBottomVisible();
   }
 
   @Test(expected = BaristaException.class)
   public void scrollsOutsideScrollableFails() throws Exception {
     assertTopVisible();
+
     scrollTo(R.id.centered_button);
+
     assertTopVisible();
   }
 
   @Test
   public void safelyScrollsInsideScrollable_byId() throws Exception {
     assertTopVisible();
+
     safelyScrollTo(R.id.really_far_away_button);
+
     assertBottomVisible();
   }
 
   @Test
   public void safelyScrollsInsideScrollable_byHardcodedText() throws Exception {
     assertTopVisible();
+
     safelyScrollTo("Really far away button");
+
     assertBottomVisible();
   }
 
   @Test
   public void safelyScrollsInsideScrollable_byTextResource() throws Exception {
     assertTopVisible();
+
     safelyScrollTo(R.string.really_far_away_button);
+
     assertBottomVisible();
   }
 
   @Test
   public void safelyScrollOutsideScrollableDoesNotFail() throws Exception {
     assertTopVisible();
+
     safelyScrollTo(R.id.centered_button);
+
     assertTopVisible();
   }
 
