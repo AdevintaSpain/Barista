25730cbdae80b91fdef97bb6842de8fea6b7b2b4
Bernat Borrás <bernatbor15@gmail.com>
2017-11-24 15:05:27 +0100

    Use Kotlin for Camera intents

---
+++ library/src/main/java/com/schibsted/spain/barista/intents/BaristaIntentMatchers.kt
@@ -0,0 +1,99 @@
+package com.schibsted.spain.barista.intents
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.net.Uri
+import android.provider.MediaStore
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull
+import com.schibsted.spain.barista.internal.failurehandler.BaristaException
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
+object BaristaIntentMatchers {
+
+    @JvmStatic
+    fun captureImage(): Matcher<Intent> {
+        return hasAction(`is`(MediaStore.ACTION_IMAGE_CAPTURE), 100, 100)
+    }
+
+    @JvmStatic
+    fun captureImage(width: Int, height: Int): Matcher<Intent> {
+        return hasAction(`is`(MediaStore.ACTION_IMAGE_CAPTURE), width, height)
+    }
+
+    private fun hasAction(actionMatcher: Matcher<String>, width: Int, height: Int): Matcher<Intent> {
+        checkNotNull(actionMatcher)
+
+        return object : TypeSafeMatcher<Intent>() {
+            override fun describeTo(description: Description) {
+                description.appendText("has action: ")
+                description.appendDescriptionOf(actionMatcher)
+            }
+
+            public override fun matchesSafely(intent: Intent): Boolean {
+                if (actionMatcher.matches(intent.action)) {
+                    val extras = intent.extras
+                    if (extras != null) {
+                        val uri = extras.getParcelable<Uri>(MediaStore.EXTRA_OUTPUT)
+
+                        try {
+                            generateBitmapOnGivenUri(uri, width, height)
+                        } catch (e: IOException) {
+                            throw BaristaException("Not able to create Bitmap at $uri", e)
+                        }
+
+                    }
+                    return true
+                }
+                return false
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun generateBitmapOnGivenUri(uri: Uri?, width: Int, height: Int) {
+        val stream = InstrumentationRegistry.getTargetContext().contentResolver.openOutputStream(uri)
+        if (stream != null) {
+            val bmp = createBitmap(width, height)
+
+            writeBitmap(stream, bmp)
+        }
+    }
+
+    private fun createBitmap(width: Int, height: Int): Bitmap {
+        val conf = Bitmap.Config.ARGB_8888
+        val bmp = Bitmap.createBitmap(width, height, conf)
+
+        val canvas = Canvas(bmp)
+
+        val paint = Paint()
+        paint.color = Color.RED
+
+        val widthPart = width / 10
+        val heightPart = height / 10
+        canvas.drawRect(widthPart.toFloat(), heightPart.toFloat(), (width - widthPart).toFloat(), (height - heightPart).toFloat(), paint)
+
+        canvas.save()
+
+        return bmp
+    }
+
+    @Throws(IOException::class)
+    private fun writeBitmap(stream: OutputStream, bmp: Bitmap) {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val bitmapData = byteArrayOutputStream.toByteArray()
+        stream.write(bitmapData)
+        stream.flush()
+        stream.close()
+    }
+}
---
+++ library/src/main/java/com/schibsted/spain/barista/intents/BaristaIntents.kt
@@ -0,0 +1,32 @@
+package com.schibsted.spain.barista.intents
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.os.Bundle
+
+import android.support.test.espresso.intent.Intents.intending
+import com.schibsted.spain.barista.intents.BaristaIntentMatchers.captureImage
+
+object BaristaIntents {
+
+    private val DEFAULT_SIZE = 100
+
+    @JvmStatic
+    @JvmOverloads
+    fun mockAndroidCamera(width: Int = DEFAULT_SIZE, height: Int = DEFAULT_SIZE) {
+        val result = createImageCaptureStub()
+        intending(captureImage(width, height)).respondWith(result)
+    }
+
+    @JvmStatic
+    private fun createImageCaptureStub(): Instrumentation.ActivityResult {
+        val resultBundle = Bundle()
+
+        val resultData = Intent()
+        resultData.putExtras(resultBundle)
+
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+    }
+
+}
