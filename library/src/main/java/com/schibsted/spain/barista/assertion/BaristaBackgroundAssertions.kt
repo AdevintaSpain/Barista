3759f7ae65d606afc93bae056df49c8a08865ab9
Roc Boronat <roc@fewlaps.com>
2017-11-27 12:22:35 +0100

    Use static imports to hide the Espresso classes and reformat to 2,4

--- library/src/main/java/com/schibsted/spain/barista/assertion/BaristaBackgroundAssertions.kt
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaBackgroundAssertions.kt
@@ -3,25 +3,27 @@ package com.schibsted.spain.barista.assertion
 import android.support.annotation.DrawableRes
 import android.support.annotation.IdRes
 import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.*
 import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.*
 import android.support.test.espresso.matcher.ViewMatchers
 import com.schibsted.spain.barista.internal.matcher.BackgroundMatcher
 import com.schibsted.spain.barista.internal.matcher.DrawableMatcher
 
 object BaristaBackgroundAssertions {
 
-    @JvmStatic
-    fun assertHasBackground(@IdRes id: Int, @DrawableRes drawable: Int) {
-        Espresso.onView(ViewMatchers.withId(id)).check(ViewAssertions.matches(BackgroundMatcher.withBackground(drawable)))
-    }
+  @JvmStatic
+  fun assertHasBackground(@IdRes id: Int, @DrawableRes drawable: Int) {
+    onView(ViewMatchers.withId(id)).check(matches(BackgroundMatcher.withBackground(drawable)))
+  }
 
-    @JvmStatic
-    fun assertHasAnyBackground(@IdRes id: Int) {
-        Espresso.onView(ViewMatchers.withId(id)).check(ViewAssertions.matches(BackgroundMatcher.withAnyBackground()))
-    }
+  @JvmStatic
+  fun assertHasAnyBackground(@IdRes id: Int) {
+    onView(ViewMatchers.withId(id)).check(matches(BackgroundMatcher.withAnyBackground()))
+  }
 
-    @JvmStatic
-    fun assertHasNoBackground(@IdRes id: Int) {
-        Espresso.onView(ViewMatchers.withId(id)).check(ViewAssertions.matches(BackgroundMatcher.withoutBackground()))
-    }
+  @JvmStatic
+  fun assertHasNoBackground(@IdRes id: Int) {
+    onView(ViewMatchers.withId(id)).check(matches(BackgroundMatcher.withoutBackground()))
+  }
 }
