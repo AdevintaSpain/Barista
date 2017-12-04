dd0c15ba552ace1af0bbaad0cd34ab24ec9af10f
jamesbluecrow <jamesbluecrow@gmail.com>
2017-12-04 16:27:55 +0000

    Added BaristaKeyboardInteractions

---
+++ library/src/main/java/com/schibsted/spain/barista/interaction/BaristaKeyboardInteractions.kt
@@ -0,0 +1,24 @@
+package com.schibsted.spain.barista.interaction
+
+import android.support.annotation.IdRes
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.pressImeActionButton
+import android.support.test.espresso.matcher.ViewMatchers.isRoot
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import com.schibsted.spain.barista.interaction.BaristaScrollInteractions.safelyScrollTo
+import com.schibsted.spain.barista.internal.matcher.DisplayedMatchers.displayedAnd
+
+object BaristaKeyboardInteractions {
+
+    @JvmStatic
+    fun close() {
+        onView(isRoot()).perform(closeSoftKeyboard())
+    }
+
+    @JvmStatic
+    fun pressImeActionButton(@IdRes editTextId: Int) {
+        safelyScrollTo(editTextId)
+        onView(displayedAnd(withId(editTextId))).perform(pressImeActionButton())
+    }
+}
