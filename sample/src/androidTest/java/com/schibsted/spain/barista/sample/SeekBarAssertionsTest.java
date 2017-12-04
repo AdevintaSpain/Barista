224d324db060c496bb615fe123a70aa2150d147f
jamesbluecrow <jamesbluecrow@gmail.com>
2017-12-04 16:36:29 +0000

    Fixed tests names

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/SeekBarAssertionsTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/SeekBarAssertionsTest.java
@@ -14,36 +14,36 @@ import static com.schibsted.spain.barista.assertion.BaristaSeekBarAssertions.ass
 @RunWith(AndroidJUnit4.class)
 public class SeekBarAssertionsTest {
 
-    @Rule public ActivityTestRule<SomeSeekBarsWithDifferentProgressesActivity> activityRule =
-            new ActivityTestRule<>(SomeSeekBarsWithDifferentProgressesActivity.class);
+  @Rule public ActivityTestRule<SomeSeekBarsWithDifferentProgressesActivity> activityRule =
+          new ActivityTestRule<>(SomeSeekBarsWithDifferentProgressesActivity.class);
 
-    @Test
-    public void checkProgress() {
-        assertProgress(R.id.seek_bar_progress_27, 27);
-    }
+  @Test
+  public void checkProgress() {
+    assertProgress(R.id.seek_bar_progress_27, 27);
+  }
 
-    @Test(expected = AssertionFailedError.class)
-    public void checkProgress_failsWhenDifferent() {
-        assertProgress(R.id.seek_bar_progress_27, 10);
-    }
+  @Test(expected = AssertionFailedError.class)
+  public void checkProgress_failsWhenDifferent() {
+    assertProgress(R.id.seek_bar_progress_27, 10);
+  }
 
-    @Test
-    public void checkMinProgress() {
-        assertProgressIsMin(R.id.seek_bar_min_progress);
-    }
+  @Test
+  public void checkMinProgress() {
+    assertProgressIsMin(R.id.seek_bar_min_progress);
+  }
 
-    @Test(expected = AssertionFailedError.class)
-    public void checkMinProgress_failsWhenProgressNotMin() {
-        assertProgress(R.id.seek_bar_min_progress, 10);
-    }
+  @Test(expected = AssertionFailedError.class)
+  public void checkMinProgress_failsWhenProgressNotMin() {
+    assertProgressIsMin(R.id.seek_bar_max_progress);
+  }
 
-    @Test
-    public void checkMaxProgress() {
-        assertProgressIsMax(R.id.seek_bar_max_progress);
-    }
+  @Test
+  public void checkMaxProgress() {
+    assertProgressIsMax(R.id.seek_bar_max_progress);
+  }
 
-    @Test(expected = AssertionFailedError.class)
-    public void checkMinProgress_failsWhenProgressNotMax() {
-        assertProgress(R.id.seek_bar_max_progress, 10);
-    }
+  @Test(expected = AssertionFailedError.class)
+  public void checkMaxProgress_failsWhenProgressNotMax() {
+    assertProgressIsMax(R.id.seek_bar_min_progress);
+  }
 }
