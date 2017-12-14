e8a6416f514327bca48b43bc1a6e94ab6dd7ef6e
Bernat Borrás Paronella <alorma@users.noreply.github.com>
2017-12-14 17:47:10 +0100

    Add with text color (#179)
    * Add assertContains()
    
    * Add contains asserts to README
    
    * Remove TextMatchers class
    
    * @Sloy comments
    
    * Add the method to check that a view doesn't contain a text also specifying its id
    
    * Add some missing tests and fix the issue that they have revealed
    
    * Remove a method that was checking that assertNotContains also checks the visibility of the View, breaking the SRP
    
    * Remove a repeated test (sorry, @alorma!)
    
    * Add tests to check that the assertion fails when the condition is not happening
    
    * Talk about TextViews instead of Buttons in the readme to avoid confusing people
    
    * Remove static class
    
    * Add text color assertions
    
    * Update readme
    
    * Rename methods, thanks @sergiandreplace
    
    * Update readme
    
    * Use color state colors
    
    * Simplified test scenario
    
    * Properly match any color state list
    
    * Add failure scenarios
    
    * Updated readme

---
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/ColorsTest.java
@@ -0,0 +1,64 @@
+package com.schibsted.spain.barista.sample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import com.schibsted.spain.barista.sample.util.FailureHandlerValidatorRule;
+import junit.framework.AssertionFailedError;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertTextColorIs;
+
+@RunWith(AndroidJUnit4.class)
+public class ColorsTest {
+
+  @Rule
+  public ActivityTestRule<ColorsActivity> activityRule = new ActivityTestRule<>(ColorsActivity.class);
+
+  @Rule
+  public FailureHandlerValidatorRule handlerValidator = new FailureHandlerValidatorRule();
+
+  @Test
+  public void checkSimpleColor() {
+    assertTextColorIs(R.id.textRed, R.color.red);
+  }
+
+  @Test
+  public void checkColorList_whenDefault() {
+    assertTextColorIs(R.id.textSelectorDefault, R.color.selector_default_disabled_checked);
+    assertTextColorIs(R.id.textSelectorDefault, R.color.defaultColor);
+  }
+
+  @Test
+  public void checkColorList_whenDisabled() {
+    assertTextColorIs(R.id.textSelectorDisabled, R.color.selector_default_disabled_checked);
+    assertTextColorIs(R.id.textSelectorDisabled, R.color.disabled);
+  }
+
+  @Test
+  public void checkColorList_whenChecked() {
+    assertTextColorIs(R.id.textSelectorChecked, R.color.selector_default_disabled_checked);
+    assertTextColorIs(R.id.textSelectorChecked, R.color.checked);
+  }
+
+  @Test(expected = AssertionFailedError.class)
+  public void checkSimpleColor_fails() {
+    assertTextColorIs(R.id.textRed, R.color.blue);
+  }
+
+  @Test(expected = AssertionFailedError.class)
+  public void checkColorList_whenDefault_fails() {
+    assertTextColorIs(R.id.textSelectorDefault, R.color.checked);
+  }
+
+  @Test(expected = AssertionFailedError.class)
+  public void checkColorList_whenDisabled_fails() {
+    assertTextColorIs(R.id.textSelectorDisabled, R.color.checked);
+  }
+
+  @Test(expected = AssertionFailedError.class)
+  public void checkColorList_whenChecked_fails() {
+    assertTextColorIs(R.id.textSelectorChecked, R.color.disabled);
+  }
+}
