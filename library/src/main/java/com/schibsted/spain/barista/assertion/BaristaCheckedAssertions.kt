31cbb874a0dc8611b68c923ab81d6bec6648380f
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-12-16 14:34:28 +0100

    Apply magicAsset on CheckedAssertions

--- library/src/main/java/com/schibsted/spain/barista/assertion/BaristaCheckedAssertions.kt
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaCheckedAssertions.kt
@@ -1,29 +1,28 @@
 package com.schibsted.spain.barista.assertion
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
-import com.schibsted.spain.barista.internal.util.resourceMatcher
+import android.support.test.espresso.matcher.ViewMatchers.isChecked
+import com.schibsted.spain.barista.internal.magicAssert
+import com.schibsted.spain.barista.internal.not
 
 object BaristaCheckedAssertions {
 
     @JvmStatic
     fun assertChecked(resId: Int) {
-        onView(resId.resourceMatcher()).check(matches(isChecked()))
+        resId magicAssert isChecked()
     }
 
     @JvmStatic
     fun assertChecked(text: String) {
-        onView(withText(text)).check(matches(isChecked()))
+        text magicAssert isChecked()
     }
 
     @JvmStatic
     fun assertUnchecked(resId: Int) {
-        onView(resId.resourceMatcher()).check(matches(isNotChecked()))
+        resId magicAssert !isChecked()
     }
 
     @JvmStatic
     fun assertUnchecked(text: String) {
-        onView(withText(text)).check(matches(isNotChecked()))
+        text magicAssert !isChecked()
     }
 }
