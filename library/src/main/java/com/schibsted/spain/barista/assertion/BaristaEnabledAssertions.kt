b1b326a567bcc8370f9da976379ff6ae4275f8ec
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-12-16 14:30:39 +0100

    Apply magicAssert to EnabledAssertions

--- library/src/main/java/com/schibsted/spain/barista/assertion/BaristaEnabledAssertions.kt
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaEnabledAssertions.kt
@@ -1,31 +1,28 @@
 package com.schibsted.spain.barista.assertion
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isEnabled
-import android.support.test.espresso.matcher.ViewMatchers.withText
-import com.schibsted.spain.barista.internal.util.resourceMatcher
-import org.hamcrest.core.IsNot.not
+import com.schibsted.spain.barista.internal.magicAssert
+import com.schibsted.spain.barista.internal.not
 
 object BaristaEnabledAssertions {
 
     @JvmStatic
     fun assertEnabled(resId: Int) {
-        onView(resId.resourceMatcher()).check(matches(isEnabled()))
+        resId magicAssert isEnabled()
     }
 
     @JvmStatic
     fun assertEnabled(text: String) {
-        onView(withText(text)).check(matches(isEnabled()))
+        text magicAssert isEnabled()
     }
 
     @JvmStatic
     fun assertDisabled(resId: Int) {
-        onView(resId.resourceMatcher()).check(matches(not(isEnabled())))
+        resId magicAssert !isEnabled()
     }
 
     @JvmStatic
     fun assertDisabled(text: String) {
-        onView(withText(text)).check(matches(not(isEnabled())))
+        text magicAssert !isEnabled()
     }
 }
