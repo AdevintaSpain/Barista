5ffefe04e2df8a94770946acfa8f887915b7f3c7
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-12-16 14:52:49 +0100

    Apply magicAssert to HintAssertions

--- library/src/main/java/com/schibsted/spain/barista/assertion/BaristaHintAssertions.kt
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaHintAssertions.kt
@@ -2,21 +2,20 @@ package com.schibsted.spain.barista.assertion
 
 import android.support.annotation.IdRes
 import android.support.annotation.StringRes
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withHint
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import com.schibsted.spain.barista.internal.magicAssert
 
 object BaristaHintAssertions {
 
     @JvmStatic
-    fun assertHint(@IdRes id: Int, @StringRes text: Int) {
-        onView(withId(id)).check(matches(withHint(text)))
+    fun assertHint(@IdRes viewId: Int, @StringRes text: Int) {
+        withId(viewId) magicAssert withHint(text)
     }
 
     @JvmStatic
-    fun assertHint(@IdRes id: Int, text: String) {
-        onView(withId(id)).check(matches(withHint(text)))
+    fun assertHint(@IdRes viewId: Int, text: String) {
+        withId(viewId) magicAssert withHint(text)
     }
 
 }
