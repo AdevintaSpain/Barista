2602b36e27728a9596b645b1b0662ab037268197
Rafa Vázquez <rafa.vazsan@gmail.com>
2018-06-27 09:58:47 +0200

    Simplified test for Barista rule

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/BaristaRuleActivityTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/BaristaRuleActivityTest.java
@@ -2,25 +2,27 @@ package com.schibsted.spain.barista.sample;
 
 import android.support.test.runner.AndroidJUnit4;
 import com.schibsted.spain.barista.rule.BaristaRule;
+import org.hamcrest.core.IsNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
-public class BaristaRuleAssertActivityTest {
+public class BaristaRuleActivityTest {
 
   @Rule
   public BaristaRule<HelloWorldActivity> baristaRule = BaristaRule.create(HelloWorldActivity.class);
 
   @Test
-  public void check_activity_is_finished() {
+  public void check_get_activity() {
     baristaRule.launchActivity();
 
-    clickOn(R.id.btn_closed);
-
-    assertTrue(baristaRule.getActivityTestRule().getActivity().isFinishing());
+    assertThat(baristaRule.getActivityTestRule().getActivity(), is(notNullValue()));
   }
 }
