baee8808874a800e076a66edd720b0a428aaa015
Rafa Vázquez <rafa.vazsan@gmail.com>
2018-06-27 11:30:55 +0200

    Remove unused imports

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/BaristaRuleActivityTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/BaristaRuleActivityTest.java
@@ -2,15 +2,12 @@ package com.schibsted.spain.barista.sample;
 
 import android.support.test.runner.AndroidJUnit4;
 import com.schibsted.spain.barista.rule.BaristaRule;
-import org.hamcrest.core.IsNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
-import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.*;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
