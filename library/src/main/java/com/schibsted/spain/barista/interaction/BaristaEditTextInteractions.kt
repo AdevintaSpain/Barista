170df8f7978720d58dfb783474600ed98abab5fe
dean <pike.dean@gmail.com>
2018-07-02 08:59:47 +1000

    Added clearText function that removes the text from a text view.

--- library/src/main/java/com/schibsted/spain/barista/interaction/BaristaEditTextInteractions.kt
+++ library/src/main/java/com/schibsted/spain/barista/interaction/BaristaEditTextInteractions.kt
@@ -2,6 +2,7 @@ package com.schibsted.spain.barista.interaction
 
 import android.support.annotation.IdRes
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.clearText
 import android.support.test.espresso.action.ViewActions.replaceText
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import com.schibsted.spain.barista.interaction.BaristaScrollInteractions.safelyScrollTo
@@ -9,9 +10,16 @@ import com.schibsted.spain.barista.internal.matcher.DisplayedMatchers.displayedA
 
 object BaristaEditTextInteractions {
 
-    @JvmStatic
-    fun writeTo(@IdRes editTextId: Int, text: String) {
-        safelyScrollTo(editTextId)
-        onView(displayedAnd(withId(editTextId))).perform(replaceText(text))
-    }
+  @JvmStatic
+  fun writeTo(@IdRes editTextId: Int, text: String) {
+    safelyScrollTo(editTextId)
+    onView(displayedAnd(withId(editTextId))).perform(replaceText(text))
+  }
+
+  @JvmStatic
+  fun clearText(@IdRes viewId: Int) {
+    safelyScrollTo(viewId)
+    onView(withId(viewId)).perform(clearText())
+  }
+
 }
