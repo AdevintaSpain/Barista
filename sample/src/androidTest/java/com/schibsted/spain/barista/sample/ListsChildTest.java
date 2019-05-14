0584d464e3c0914d9a0709dc032b884c7916df9f
gdomingues <germanno.domingues@gmail.com>
2019-05-14 13:33:46 +0200

    Rename test class focused on list child actions

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/ListsChildTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/ListsChildTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 @RunWith(AndroidJUnit4.class)
-public class ListsChildClickTest {
+public class ListsChildTest {
 
   @Rule
   public ActivityTestRule<ListsActivity> activity = new ActivityTestRule<>(ListsActivity.class, true, false);
