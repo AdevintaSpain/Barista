948136ec27c903e526feebd56ea00058a64f7820
gdomingues <germanno.domingues@gmail.com>
2019-05-14 15:52:23 +0200

    Remove wildcard import

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/ListsChildTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/ListsChildTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
