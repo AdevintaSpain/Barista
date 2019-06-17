2b8b2d198c34936080163b573962891d87d617b5
Bernat Borrás <bernat.borras@schibsted.com>
2019-06-17 11:39:13 +0200

    Create content description matcher

--- library/src/main/java/com/schibsted/spain/barista/assertion/BaristaAssistiveTextAssertions.kt
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaAssistiveTextAssertions.kt
@@ -3,8 +3,8 @@ package com.schibsted.spain.barista.assertion
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.annotation.StringRes
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
 import com.google.android.material.textfield.TextInputLayout
 import com.schibsted.spain.barista.internal.assertAny
 import org.hamcrest.Description
@@ -14,8 +14,8 @@ import org.hamcrest.TypeSafeMatcher
 object BaristaAssistiveTextAssertions {
   @JvmStatic
   fun assertAssistiveText(@IdRes viewId: Int, @StringRes text: Int) {
-    val resourceString = InstrumentationRegistry.getTargetContext().resources.getString(text)
-    assertAssistiveText(viewId, resourceString)
+    val resourceString = InstrumentationRegistry.getInstrumentation().targetContext.resources.getString(text)
+      assertAssistiveText(viewId, resourceString)
   }
 
   @JvmStatic
---
+++ library/src/main/java/com/schibsted/spain/barista/assertion/BaristaContentDescriptionAssertions.kt
@@ -0,0 +1,36 @@
+package com.schibsted.spain.barista.assertion
+
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.annotation.StringRes
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import com.schibsted.spain.barista.internal.assertAny
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+object BaristaContentDescriptionAssertions {
+  @JvmStatic
+  fun assertContentDescription(@IdRes viewId: Int, @StringRes text: Int) {
+    val resourceString = InstrumentationRegistry.getInstrumentation().targetContext.resources.getString(text)
+    assertContentDescription(viewId, resourceString)
+  }
+
+  @JvmStatic
+  fun assertContentDescription(@IdRes viewId: Int, text: String) {
+    ViewMatchers.withId(viewId).assertAny(matchContentDescription(text))
+  }
+
+  private fun matchContentDescription(expectedContentDescription: String): Matcher<View> {
+    return object : TypeSafeMatcher<View>() {
+      override fun describeTo(description: Description) {
+        description.appendText("with content description: ").appendText(expectedContentDescription)
+      }
+
+      override fun matchesSafely(item: View): Boolean {
+        return item.contentDescription == expectedContentDescription
+      }
+    }
+  }
+}
