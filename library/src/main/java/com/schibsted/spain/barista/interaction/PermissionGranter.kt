b24b05981f8d09df0db7316d97d1c1262ae17c0b
Bernat Borrás <bernat.borras@schibsted.com>
2019-07-19 09:11:20 +0200

    Convert to kotlin

---
+++ library/src/main/java/com/schibsted/spain/barista/interaction/PermissionGranter.kt
@@ -0,0 +1,48 @@
+package com.schibsted.spain.barista.interaction
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObjectNotFoundException
+import androidx.test.uiautomator.UiSelector
+import com.schibsted.spain.barista.interaction.BaristaSleepInteractions.sleepThread
+
+object PermissionGranter {
+
+    private val PERMISSIONS_DIALOG_DELAY = 3000
+    private val PERMISSIONS_DIALOG_ALLOW_ID = "com.android.packageinstaller:id/permission_allow_button"
+    //    private static final String PERMISSIONS_DIALOG_DENY_ID = "com.android.packageinstaller:id/permission_deny_button";
+
+    @JvmStatic
+    fun allowPermissionsIfNeeded(permissionNeeded: String) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !hasNeededPermission(ApplicationProvider.getApplicationContext(),
+                            permissionNeeded)) {
+                sleepThread(PERMISSIONS_DIALOG_DELAY.toLong())
+                val device = UiDevice.getInstance(getInstrumentation())
+                val allowPermissions = device.findObject(UiSelector()
+                        .clickable(true)
+                        .checkable(false)
+                        .resourceId(PERMISSIONS_DIALOG_ALLOW_ID))
+                if (allowPermissions.exists()) {
+                    allowPermissions.click()
+                }
+            }
+        } catch (e: UiObjectNotFoundException) {
+            Log.e("Barista", "There is no permissions dialog to interact with", e)
+        }
+    }
+
+    private fun hasNeededPermission(context: Context, permissionNeeded: String): Boolean {
+        val permissionStatus = checkSelfPermission(context, permissionNeeded)
+        return permissionStatus == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkSelfPermission(context: Context, permission: String): Int {
+        return context.checkPermission(permission, android.os.Process.myPid(), android.os.Process.myUid())
+    }
+}
