3398dd39ea96633650b7dfc16b7a02df8aeeaa2f
gdomingues <germanno.domingues@gmail.com>
2019-08-05 16:03:26 +0200

    Set typed text to result field whenever EditText content changes

--- sample/src/main/java/com/schibsted/spain/barista/sample/TextListViewAdapter.java
+++ sample/src/main/java/com/schibsted/spain/barista/sample/TextListViewAdapter.java
@@ -2,21 +2,25 @@ package com.schibsted.spain.barista.sample;
 
 import android.app.Activity;
 import android.content.Context;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TextListViewAdapter extends BaseAdapter {
   private Activity activity;
   private String[] items;
   private final TextView clickedResult;
+  private final TextWatcher textWatcher;
 
-  TextListViewAdapter(Activity activity, String[] items, TextView clickedResult) {
+  TextListViewAdapter(Activity activity, String[] items, TextView clickedResult, TextWatcher textWatcher) {
     this.activity = activity;
     this.items = items.clone();
     this.clickedResult = clickedResult;
+    this.textWatcher = textWatcher;
   }
 
   @Override
@@ -46,6 +50,8 @@ public class TextListViewAdapter extends BaseAdapter {
     TextView textView = (TextView) rowView.findViewById(R.id.textview);
     View yesButton = rowView.findViewById(R.id.yes);
     View noButton = rowView.findViewById(R.id.no);
+    EditText editText = rowView.findViewById(R.id.edittext);
+
     textView.setText(items[position]);
 
     rowView.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +72,10 @@ public class TextListViewAdapter extends BaseAdapter {
         clickedResult.setText("no");
       }
     });
+
+    // Avoiding adding TextWatcher multiple times
+    editText.removeTextChangedListener(textWatcher);
+    editText.addTextChangedListener(textWatcher);
 
     return rowView;
   }
--- sample/src/main/java/com/schibsted/spain/barista/sample/TextRecyclerViewAdapter.java
+++ sample/src/main/java/com/schibsted/spain/barista/sample/TextRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.schibsted.spain.barista.sample;
 
 import androidx.recyclerview.widget.RecyclerView;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static com.schibsted.spain.barista.sample.ListsActivity.getRecyclerViewTextAt;
@@ -11,10 +13,12 @@ import static com.schibsted.spain.barista.sample.ListsActivity.getRecyclerViewTe
 public class TextRecyclerViewAdapter extends RecyclerView.Adapter<TextRecyclerViewAdapter.ViewHolder> {
   private final String[] items;
   private final TextView clickedResult;
+  private final TextWatcher textWatcher;
 
-  TextRecyclerViewAdapter(String[] items, TextView clickedResult) {
+  TextRecyclerViewAdapter(String[] items, TextView clickedResult, TextWatcher textWatcher) {
     this.items = items.clone();
     this.clickedResult = clickedResult;
+    this.textWatcher = textWatcher;
   }
 
   public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -22,7 +26,8 @@ public class TextRecyclerViewAdapter extends RecyclerView.Adapter<TextRecyclerVi
     TextView textView = (TextView) root.findViewById(R.id.textview);
     View yesButton = root.findViewById(R.id.yes);
     View noButton = root.findViewById(R.id.no);
-    return new ViewHolder(root, textView, yesButton, noButton);
+    EditText editText = root.findViewById(R.id.edittext);
+    return new ViewHolder(root, textView, yesButton, noButton, editText, textWatcher);
   }
 
   public void onBindViewHolder(final ViewHolder holder, final int position) {
@@ -55,12 +60,18 @@ public class TextRecyclerViewAdapter extends RecyclerView.Adapter<TextRecyclerVi
     TextView textView;
     View yesButton;
     View noButton;
+    EditText editText;
 
-    ViewHolder(View root, TextView textView, View yesButton, View noButton) {
+    ViewHolder(View root, TextView textView, View yesButton, View noButton, EditText editText, TextWatcher textWatcher) {
       super(root);
       this.textView = textView;
       this.yesButton = yesButton;
       this.noButton = noButton;
+      this.editText = editText;
+
+      // Avoiding adding TextWatcher multiple times
+      this.editText.removeTextChangedListener(textWatcher);
+      this.editText.addTextChangedListener(textWatcher);
     }
   }
 }
