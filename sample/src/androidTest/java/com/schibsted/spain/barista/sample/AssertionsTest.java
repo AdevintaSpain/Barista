7ed69125a07041dbd7e3c60dafde5399b61aea26
Bernat Borrás <bernat.borras@schibsted.com>
2019-08-07 10:44:23 +0200

    Add test to check set vector on runtime

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/AssertionsTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/AssertionsTest.java
@@ -258,9 +258,15 @@ public class AssertionsTest {
   public void checkDrawable_withId() throws Exception {
     assertHasDrawable(R.id.image_view, R.drawable.ic_barista);
   }
+
   @Test
   public void checkVectorDrawable_withId() throws Exception {
     assertHasDrawable(R.id.vector_image_view, R.drawable.barista_logo_vector);
+  }
+
+  @Test
+  public void checkVectorDrawableFromCode_withId() throws Exception {
+    assertHasDrawable(R.id.code_vector_image_view, R.drawable.barista_logo_vector);
   }
 
   @Test(expected = BaristaException.class)
@@ -276,6 +282,11 @@ public class AssertionsTest {
   @Test
   public void checkVectorDrawable_withAnyDrawable() throws Exception {
     assertHasAnyDrawable(R.id.vector_image_view);
+  }
+
+  @Test
+  public void checkVectorDrawableFromCode_withAnyDrawable() throws Exception {
+    assertHasAnyDrawable(R.id.code_vector_image_view);
   }
 
   @Test(expected = BaristaException.class)
