28418f43c3e647fc0601570736fa217ad3763a3d
Bernat Borrás <bernat.borras@schibsted.com>
2019-12-03 08:45:10 +0100

    Use Flaky instead of ignore

--- sample/src/androidTest/java/com/schibsted/spain/barista/sample/MenuClickTest.java
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/MenuClickTest.java
@@ -1,10 +1,10 @@
 package com.schibsted.spain.barista.sample;
 
+import androidx.test.filters.FlakyTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import com.schibsted.spain.barista.internal.failurehandler.BaristaException;
 import com.schibsted.spain.barista.sample.util.FailureHandlerValidatorRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,21 +47,21 @@ public class MenuClickTest {
   }
 
   @Test
-  @Ignore
+  @FlakyTest(detail = "This tests passes on local, but fails on CI")
   public void overflowMenuClick_byId() {
     clickMenu(R.id.menu_action_3);
     assertDisplayed("Third menu option");
   }
 
   @Test
-  @Ignore
+  @FlakyTest(detail = "This tests passes on local, but fails on CI")
   public void overflowMenuClick_byTitle() {
     clickMenu("Menu 3");
     assertDisplayed("Third menu option");
   }
 
   @Test
-  @Ignore
+  @FlakyTest(detail = "This tests passes on local, but fails on CI")
   public void openOverflowMenu_withoutClickingAnyOption() {
     openMenu();
     assertDisplayed("Menu 3");
