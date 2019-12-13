13fcea241f1825ab8bb08edc4eeea2ed7d072658
galioglucem <cem@eventmatchapp.com>
2019-12-13 19:47:34 +0100

    Add tests for atPosition
    Tests added in this commit shows why and how the method atPosition has bugs.
    The expected exception is there to let the test succeed before I fix the bug.
    I will remove the exception when the bug is fixed

---
+++ sample/src/androidTest/java/com/schibsted/spain/barista/sample/AtPositionTest.java
@@ -0,0 +1,50 @@
+package com.schibsted.spain.barista.sample;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.schibsted.spain.barista.internal.failurehandler.BaristaException;
+import com.schibsted.spain.barista.internal.matcher.HelperMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
+
+@RunWith(AndroidJUnit4.class)
+public class AtPositionTest {
+
+    private static final int THIRD_POSITION = 2;
+
+    @Rule
+    public ActivityTestRule<TestAtPositionActivity> activityRule =
+            new ActivityTestRule<>(TestAtPositionActivity.class);
+
+
+    /**
+     * This test succeeds even when the counter used in atPosition is not set back to 0
+     */
+    @Test
+    public void useViewElementFoundWithAtPositionOnce() {
+       Matcher<View> thirdTextView = HelperMatchers.atPosition(THIRD_POSITION, ViewMatchers.isAssignableFrom(TextView.class));
+       assertDisplayed(thirdTextView);
+    }
+
+    /**
+     * This test fails if the counter used in atPosition is not set back to 0
+     */
+    @Test (expected = BaristaException.class)
+    public void useViewElementFoundWithAtPositionTwice() {
+        Matcher<View> thirdTextView = HelperMatchers.atPosition(THIRD_POSITION, ViewMatchers.isAssignableFrom(TextView.class));
+        assertDisplayed(thirdTextView);
+        assertDisplayed(thirdTextView);
+    }
+
+}
---
+++ sample/src/main/java/com/schibsted/spain/barista/sample/TestAtPositionActivity.java
@@ -0,0 +1,13 @@
+package com.schibsted.spain.barista.sample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestAtPositionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_at_position);
+    }
+}
