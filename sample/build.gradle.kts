8c7dfe837415f6b2328c8a561ad2cfc500bc8d34
Bernat Borrás <bernat.borras@adevinta.com>
2021-09-09 14:47:25 +0200

    Remove unneded files

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
   id("com.android.application")
   kotlin("android")
   id("kotlin-android-extensions")
-  id("kotlin-android")
 }
 
 apply(from = "../config/android-quality.gradle")
@@ -11,7 +10,6 @@ android {
   compileSdk = 30
 
   defaultConfig {
-    testInstrumentationRunnerArguments(mapOf())
     minSdk = 21
     targetSdk = 30
 
@@ -29,9 +27,6 @@ android {
   testOptions {
     execution = "ANDROIDX_TEST_ORCHESTRATOR"
   }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
 }
 
 dependencies {
@@ -43,14 +38,13 @@ dependencies {
   implementation("com.github.bumptech.glide:glide:4.10.0")
   implementation("com.google.android.material:material:1.2.0")
   implementation("androidx.core:core-ktx:1.0.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
 
   androidTestUtil("androidx.test:orchestrator:1.3.0")
   androidTestImplementation(project(":library"))
-  androidTestImplementation(project(":barista-compose"))
   androidTestImplementation("org.assertj:assertj-core:2.9.1")
   androidTestImplementation("com.nhaarman:mockito-kotlin:1.5.0")
   androidTestImplementation("org.mockito:mockito-android:2.28.2")
 
   testImplementation("junit:junit:4.12")
+
 }
