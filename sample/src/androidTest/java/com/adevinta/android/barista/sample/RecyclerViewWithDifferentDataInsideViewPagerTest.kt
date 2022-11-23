eb448468e96480cf59421911c51392ebce5aad6e
Quentin MOUTY <quentin.mouty@corp.ovh.com>
2022-11-23 18:46:42 +0100

    Convert RecyclerViewWithDifferentDataInsideViewPagerTest to kotlin

--- sample/src/androidTest/java/com/adevinta/android/barista/sample/RecyclerViewWithDifferentDataInsideViewPagerTest.kt
+++ sample/src/androidTest/java/com/adevinta/android/barista/sample/RecyclerViewWithDifferentDataInsideViewPagerTest.kt
@@ -1,30 +1,26 @@
-package com.adevinta.android.barista.sample;
+package com.adevinta.android.barista.sample
 
-import androidx.test.rule.ActivityTestRule;
-import org.junit.Rule;
-import org.junit.Test;
+import androidx.test.rule.ActivityTestRule
+import com.adevinta.android.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
+import com.adevinta.android.barista.interaction.BaristaSleepInteractions
+import com.adevinta.android.barista.interaction.BaristaViewPagerInteractions.swipeViewPagerBack
+import com.adevinta.android.barista.interaction.BaristaViewPagerInteractions.swipeViewPagerForward
+import org.junit.Rule
+import org.junit.Test
 
-import static com.adevinta.android.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition;
-import static com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep;
-import static com.adevinta.android.barista.interaction.BaristaViewPagerInteractions.swipeViewPagerBack;
-import static com.adevinta.android.barista.interaction.BaristaViewPagerInteractions.swipeViewPagerForward;
+class RecyclerViewWithDifferentDataInsideViewPagerTest {
+    @Rule
+    var activityRule = ActivityTestRule(
+        RecyclerViewsWithDifferentDataInsideViewPagerActivity::class.java
+    )
 
-public class RecyclerViewWithDifferentDataInsideViewPagerTest {
-
-  @Rule
-  public ActivityTestRule<RecyclerViewsWithDifferentDataInsideViewPagerActivity> activityRule =
-      new ActivityTestRule<>(RecyclerViewsWithDifferentDataInsideViewPagerActivity.class);
-
-  @Test
-  public void checkClickRecyclerViewItem() {
-    swipeViewPagerForward(R.id.pager);
-    sleep(500);
-
-    assertDisplayedAtPosition(R.id.recycler, 1, "Marionberry");
-
-    swipeViewPagerBack(R.id.pager);
-    sleep(500);
-
-    assertDisplayedAtPosition(R.id.recycler, 0, R.id.textview, "Apple");
-  }
+    @Test
+    fun checkClickRecyclerViewItem() {
+        swipeViewPagerForward(R.id.pager)
+        BaristaSleepInteractions.sleep(500)
+        assertDisplayedAtPosition(R.id.recycler, 1, "Marionberry")
+        swipeViewPagerBack(R.id.pager)
+        BaristaSleepInteractions.sleep(500)
+        assertDisplayedAtPosition(R.id.recycler, 0, R.id.textview, "Apple")
+    }
 }
