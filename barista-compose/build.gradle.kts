ce3d2ad3d2b8201ad63f52a70f41a030713655c1
Bernat Borrás <bernat.borras@adevinta.com>
2022-12-03 14:47:02 +0100

    Update barista-compose dependencies

--- barista-compose/build.gradle.kts
+++ barista-compose/build.gradle.kts
@@ -36,16 +36,16 @@ android {
 }
 
 dependencies {
-  api("androidx.test.ext:junit:1.1.3")
-  api("androidx.compose.ui:ui-test-junit4:1.0.2")
+  api(libs.androidX.test.junit)
+  api(libs.androidX.compose.test.ui.junit4)
 
-  debugImplementation("androidx.compose.ui:ui:${libs.versions.compose.get()}")
-  debugImplementation("androidx.compose.material:material:${libs.versions.compose.get()}")
-  debugImplementation("androidx.compose.ui:ui-tooling-preview:${libs.versions.compose.get()}")
-  debugImplementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-  debugImplementation("androidx.activity:activity-compose:1.3.1")
-  debugImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
-  debugImplementation("androidx.compose.ui:ui-test-manifest:${libs.versions.compose.get()}")
-  debugImplementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
+  debugImplementation(libs.androidX.compose.ui.ui)
+  debugImplementation(libs.androidX.compose.material)
+  debugImplementation(libs.androidX.compose.ui.tooling.preview)
+  debugImplementation(libs.androidX.compose.ui.tooling.core)
+  debugImplementation(libs.androidX.compose.test.ui.manifest)
+
+  debugImplementation(libs.androidX.lifecycle)
+  debugImplementation(libs.androidX.activity.compose)
 
 }
