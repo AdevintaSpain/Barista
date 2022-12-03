a258a3bb34357a4288db1fda5724503f8a720e37
Bernat Borrás <bernat.borras@adevinta.com>
2022-12-03 13:57:42 +0100

    Make build.gradle compose compile

--- barista-compose/build.gradle.kts
+++ barista-compose/build.gradle.kts
@@ -19,12 +19,13 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     testInstrumentationRunnerArguments["clearPackageData"] = "true"
   }
-  
+
   buildFeatures {
     compose = true
   }
+
   composeOptions {
-    kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+    kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
   }
 
   packagingOptions {
@@ -38,13 +39,13 @@ dependencies {
   api("androidx.test.ext:junit:1.1.3")
   api("androidx.compose.ui:ui-test-junit4:1.0.2")
 
-  debugImplementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-  debugImplementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-  debugImplementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+  debugImplementation("androidx.compose.ui:ui:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.material:material:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.ui:ui-tooling-preview:${libs.versions.compose.get()}")
   debugImplementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
   debugImplementation("androidx.activity:activity-compose:1.3.1")
-  debugImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
-  debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
-  debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+  debugImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.ui:ui-test-manifest:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
 
 }
