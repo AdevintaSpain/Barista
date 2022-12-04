bdb42713649a8636740b3e707f937750a2c23126
Bernat Borrás <bernat.borras@adevinta.com>
2022-12-04 15:31:28 +0100

    Migrate lintOptions to lint

--- barista/build.gradle.kts
+++ barista/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  lintOptions {
+  lint {
     disable.add("InvalidPackage")
   }
 }
@@ -28,18 +28,18 @@ dependencies {
   api(libs.androidX.test.espresso.contrib)
   api(libs.androidX.test.uiAutomator)
   api(libs.androidX.test.espresso.intents)
-  
+
   implementation(libs.androidX.annotation)
   implementation(libs.androidX.legacy.support)
   implementation(libs.androidX.vectorDrawable.animated)
   implementation(libs.androidX.recyclerView)
   api(libs.androidX.viewPager2)
-  
+
   implementation(libs.androidX.material)
-  
+
   testImplementation(libs.testing.jUnit)
   testImplementation(libs.testing.jUnitParams)
   testImplementation(libs.testing.assertJ)
-  
+
   testImplementation(libs.testing.mockito.core)
 }
