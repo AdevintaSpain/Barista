032f13f09712f85bcaa434f401085b2db70e1e04
Bernat Borrás <bernat.borras@adevinta.com>
2022-12-04 12:07:11 +0100

    Move package

--- barista-compose/build.gradle.kts
+++ barista-compose/build.gradle.kts
@@ -32,7 +32,7 @@ android {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
   }
-  namespace = "com.alorma.barista_compose"
+  namespace = "com.adevinta.android.barista.compose"
 }
 
 dependencies {
