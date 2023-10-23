4e22a7dd655b3a235a03b0c9b8d8a940a7964114
Bernat Borrás <bernat.borras@adevinta.com>
2023-10-23 09:50:28 +0200

    Remove forcing

--- barista/build.gradle.kts
+++ barista/build.gradle.kts
@@ -24,12 +24,6 @@ android {
   }
 }
 
-configurations.all {
-  resolutionStrategy.force("org.hamcrest:hamcrest:2.2")
-  resolutionStrategy.force("org.hamcrest:hamcrest-library:2.2")
-  resolutionStrategy.force("org.hamcrest:hamcrest-core:2.2")
-}
-
 dependencies {
   api(libs.androidX.test.espresso.core)
   api(libs.androidX.test.espresso.contrib)
